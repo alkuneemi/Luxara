70182563c6c9c8eee6c0b145e4dcda562afc95c2
abd-msyukyu-odoo <abd@odoo.com>
2025-11-10 14:20:47 +0100

    TODO email_image_plugin (convert font to image todo)

--- addons/mail/static/src/convert_inline/plugins/email_image_plugin.js
+++ addons/mail/static/src/convert_inline/plugins/email_image_plugin.js
@@ -2,12 +2,23 @@ import { registry } from "@web/core/registry";
 import { Plugin } from "../plugin";
 import { getImageSrc } from "@html_editor/utils/image";
 import { loadImage } from "@html_editor/utils/image_processing";
+import { useShorthands } from "@mail/convert_inline/plugins/hooks";
 
 export class EmailImagePlugin extends Plugin {
     static id = "image";
+    static dependencies = ["computeStyle"];
     resources = {
         load_reference_content_handlers: () => this.loadImages(this.config.reference),
     };
+
+    setup() {
+        useShorthands(this, "computeStyle", [
+            "getComputedStyle",
+            "getHeight",
+            "getStylePropertyValue",
+            "getWidth",
+        ]);
+    }
 
     /**
      * Return promises for every image to control when they have their final
