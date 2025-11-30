5c8270b1d76aab7137094e66dcafadc9d4ad93da
Rémy Voet (ryv) <ryv@odoo.com>
2025-11-30 12:08:14 +0100

    [IMP] core: remove exists from ref

--- addons/test_website_modules/tests/test_performance.py
+++ addons/test_website_modules/tests/test_performance.py
@@ -295,7 +295,7 @@ class TestWebsiteAllPerformance(TestWebsitePerformanceCommon, TestWebsitePriceLi
         query_count = 41  # To increase this number you must ask the permission to al
         queries = {
             'website': 1,
-            'res_company': 2,
+            'res_company': 1,
             'product_pricelist': 4,
             'product_template': 3,
             'product_tag': 1,
