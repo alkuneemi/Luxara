6dbcf082826ac3e3c43e71fdacf7ae6bf50762f1
dija-odoo <dija@odoo.com>
2026-02-05 10:16:22 +0530

    [IMP] payment_custom, website_sale: make pending payment instructions generic
    Change the way pending payment instructions works for `wire_transfer`.
    
    - Introduce generic template for instructions to be displayed after the
      payment completion
    - Choose specific bank account to show payment instructions
    - Allow easy extension for other custom modes (e.g. SEPA)
    
    task-4975272

--- addons/payment/models/payment_provider.py
+++ addons/payment/models/payment_provider.py
@@ -707,6 +707,14 @@ class PaymentProvider(models.Model):
 
         return providers
 
+    def _get_pending_msg(self, **_kwargs):
+        """Return the message when the payment is pending after processing.
+
+        :return: The pending message.
+        :rtype: str
+        """
+        return self.pending_msg
+
     def _is_tokenization_required(self, **_kwargs):
         """Return whether tokenizing the transaction is required given its context.
 
---
+++ addons/website_sale/models/payment_provider.py
@@ -0,0 +1,20 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo import models
+
+
+class PaymentProvider(models.Model):
+    _inherit = "payment.provider"
+
+    # === BUSINESS METHODS === #
+
+    def _get_pending_msg(self, **kwargs):
+        """Override to return a specific pending message for website orders."""
+        if (
+            kwargs.get("from_website")
+            and self.code == "custom"
+            and self.custom_mode in self._get_custom_bank_related_modes()
+        ):
+            return self.env._("Your order will be confirmed after payment is received.")
+
+        return super()._get_pending_msg(**kwargs)
