b51af7dda4ff88739e05838e98bc2fa00869687e
Laurent Smet (LAS) <las@odoo.com>
2026-02-20 14:09:05 +0100

    [IMP] account_edi_ubl_cii: Introduce the PINT layer
    Add the layer PINT between UBL and BIS3.
    
    task: 5890887
    
    Co-authored-by: Yosua Nicolaus <yoni@odoo.com>

---
+++ addons/l10n_ro_edi/models/account_edi_ubl_pint_eu_ro.py
@@ -0,0 +1,110 @@
+from odoo import models
+from odoo.addons.account_edi_ubl_cii.models.account_edi_common import documents
+from odoo.addons.l10n_ro_edi.models.account_edi_xml_ubl_ciusro import get_formatted_sector_ro, _has_vat
+
+DEFAULT_VAT = '0000000000000'
+
+
+class AccountEdiUBLPintRO(models.AbstractModel):
+    _name = "account.edi.ubl_pint_eu_ro"
+    _inherit = "account.edi.ubl_pint_eu"
+    _description = "UBL PINT RO Common"
+
+    @property
+    def python_class(self):
+        return AccountEdiUBLPintRO
+
+    @documents('invoice', 'credit_note')
+    def _ubl_add_customization_id_node__pint_eu_ro(self, vals):
+        super()._ubl_add_customization_id_node(vals)
+        vals['document_node']['cbc:CustomizationID']['_text'] = 'urn:cen.eu:en16931:2017#compliant#urn:efactura.mfinante.ro:CIUS-RO:1.0.1'
+
+    @documents('invoice', 'credit_note')
+    def _ubl_add_tax_currency_code_node__pint_eu_ro(self, vals):
+        self._ubl_add_tax_currency_code_node_company_currency(vals)
+
+    @documents('invoice', 'credit_note')
+    def _ubl_get_partner_address_node__pint_eu_ro(self, vals, partner):
+        node = super()._ubl_get_partner_address_node(vals, partner)
+
+        if not partner.state_id:
+            return node
+
+        node['cbc:CountrySubentity']['_text'] = f'{partner.country_code}-{partner.state_id.code}'
+
+        # Romania requires the CityName to be in the format of "SECTORX" if the address state is in Bucharest.
+        if partner.state_id.code == 'B' and partner.city:
+            node['cbc:CityName']['_text'] = get_formatted_sector_ro(partner.city)
+
+        return node
+
+    @documents('invoice', 'credit_note')
+    def _ubl_add_accounting_supplier_party_tax_scheme_nodes__pint_eu_ro(self, vals):
+        super()._ubl_add_accounting_supplier_party_tax_scheme_nodes(vals)
+        partner = vals['party_vals']['partner']
+        commercial_partner = partner.commercial_partner_id
+        company_id = None
+
+        if not _has_vat(commercial_partner.vat):
+            if commercial_partner.company_registry:
+                company_id = commercial_partner.company_registry
+        else:
+            company_id = commercial_partner.vat
+
+        vals['party_node']['cac:PartyTaxScheme'] = [{
+            'cbc:CompanyID': {'_text': company_id},
+            'cac:TaxScheme': {
+                'cbc:ID': {'_text': 'VAT' if company_id[:2].isalpha() else 'NOT_EU_VAT'},
+            },
+        }] if company_id else []
+
+    @documents('invoice', 'credit_note')
+    def _ubl_add_accounting_supplier_party_legal_entity_nodes__pint_eu_ro(self, vals):
+        super()._ubl_add_accounting_supplier_party_legal_entity_nodes(vals)
+        partner = vals['party_vals']['partner']
+        commercial_partner = partner.commercial_partner_id
+
+        if not _has_vat(commercial_partner.vat):
+            if commercial_partner.company_registry:
+                vals['party_node']['cac:PartyLegalEntity'] = [{
+                    'cbc:RegistrationName': {'_text': commercial_partner.name},
+                    'cbc:CompanyID': {
+                        '_text': commercial_partner.company_registry,
+                        'schemeID': None,
+                    },
+                }]
+            else:
+                vals['party_node']['cac:PartyLegalEntity'] = []
+
+    @documents('invoice', 'credit_note')
+    def _ubl_add_accounting_customer_party_tax_scheme_nodes__pint_eu_ro(self, vals):
+        super()._ubl_add_accounting_customer_party_tax_scheme_nodes(vals)
+        partner = vals['party_vals']['partner']
+        commercial_partner = partner.commercial_partner_id
+
+        if not _has_vat(commercial_partner.vat):
+            company_id = DEFAULT_VAT
+        else:
+            company_id = commercial_partner.vat
+
+        vals['party_node']['cac:PartyTaxScheme'] = [{
+            'cbc:CompanyID': {'_text': company_id},
+            'cac:TaxScheme': {
+                'cbc:ID': {'_text': 'VAT' if company_id[:2].isalpha() else 'NOT_EU_VAT'},
+            },
+        }] if company_id else []
+
+    @documents('invoice', 'credit_note')
+    def _ubl_add_accounting_customer_party_legal_entity_nodes__pint_eu_ro(self, vals):
+        super()._ubl_add_accounting_customer_party_legal_entity_nodes(vals)
+        partner = vals['party_vals']['partner']
+        commercial_partner = partner.commercial_partner_id
+
+        if not _has_vat(commercial_partner.vat):
+            vals['party_node']['cac:PartyLegalEntity'] = [{
+                'cbc:RegistrationName': {'_text': commercial_partner.name},
+                'cbc:CompanyID': {
+                    '_text': DEFAULT_VAT,
+                    'schemeID': None,
+                },
+            }]
