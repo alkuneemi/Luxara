9a63e05c749bbcc1521643a902d6ef59e2ad9baf
Robin Lejeune (role) <role@odoo.com>
2026-02-23 16:41:33 +0100

    [IMP] website: use better aria-label for carousel indicators
    All the carousel indicators used `Carousel indicator` as their
    `aria-label`. While this may be better than nothing, it doesn't give any
    information and is even redundant and annoying if a user relies on that
    to browse the page.
    
    We will use "Slide X of Y" as it seems to be a best practice in
    accessibility (see [WAI-ARIA carousel pattern]).
    
    Most "Previous" / "Next" carousel arrows' labels are also adapted to
    include mention of "Previous slide" / "Next slide". This makes it
    clearer for users relying on assistive technology, for whom the context
    of the element that is read may not be obvious.
    
    [WAI-ARIA carousel pattern]: https://www.w3.org/WAI/ARIA/apg/patterns/carousel/examples/carousel-2-tablist/
    
    task-5470023

--- addons/website/static/src/builder/plugins/options/image_gallery_option_plugin.js
+++ addons/website/static/src/builder/plugins/options/image_gallery_option_plugin.js
@@ -1,11 +1,12 @@
 import { registry } from "@web/core/registry";
 import { Plugin } from "@html_editor/plugin";
 import { loadImageInfo } from "@html_editor/utils/image_processing";
-import { renderToElement } from "@web/core/utils/render";
 import { updateCarouselIndicators } from "../carousel_option_plugin";
 import { BuilderAction } from "@html_builder/core/builder_action";
 import { forwardToThumbnail } from "@html_builder/utils/utils_css";
 import { ClassAction } from "@html_builder/core/core_builder_action_plugin";
+import { _t } from "@web/core/l10n/translation";
+import { renderToElement } from "@web/core/utils/render";
 import { uuid } from "@web/core/utils/strings";
 
 /**
@@ -276,6 +277,8 @@ export class ImageGalleryOptionPlugin extends Plugin {
             id: "slideshow_" + new Date().getTime(),
             colorContrast,
             copyAttributes: true,
+            getIndicatorLabel: (itemPosition, total) =>
+                _t("Slide %(itemPosition)s of %(total)s", { itemPosition, total }),
         });
         if (carouselEl) {
             carouselEl.removeEventListener("slid.bs.carousel", this.onCarouselSlid);
