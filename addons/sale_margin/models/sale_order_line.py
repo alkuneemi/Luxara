c4ccd88f851e5d999cff86fa5cd1159d96a12cef
livaios <liew@odoo.com>
2026-02-23 14:28:36 +0100

    [IMP] sale_margin: make margin editable
    Make Margin & Margin % editable on SOL.
    
    Impact it on Unit Price as if you were editing it manually. 
    
    task-5956031

--- addons/sale_margin/models/sale_order_line.py
+++ addons/sale_margin/models/sale_order_line.py
@@ -1,6 +1,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
+from odoo.exceptions import UserError
 
 
 class SaleOrderLine(models.Model):
@@ -10,6 +11,7 @@ class SaleOrderLine(models.Model):
         "Margin",
         compute="_compute_margin",
         min_display_digits="Product Price",
+        readonly=False,
         store=True,
         groups="base.group_user",
         precompute=True,
@@ -17,6 +19,7 @@ class SaleOrderLine(models.Model):
     margin_percent = fields.Float(
         "Margin (%)",
         compute="_compute_margin",
+        readonly=False,
         store=True,
         groups="base.group_user",
         precompute=True,
@@ -60,3 +63,31 @@ class SaleOrderLine(models.Model):
             else:
                 line.margin = line.price_subtotal - (line.purchase_price * line.product_uom_qty)
                 line.margin_percent = line.price_subtotal and line.margin / line.price_subtotal
+
+    @api.onchange('margin')
+    def _onchange_margin(self):
+        for line in self:
+            if line.qty_delivered and not line.product_uom_qty:
+                line.price_unit = line.margin / line.qty_delivered + line.purchase_price
+            elif line.product_uom_qty:
+                line.price_unit = line.margin / line.product_uom_qty + line.purchase_price
+            line.margin_percent = line.price_unit and 1 - line.purchase_price / line.price_unit
+
+    @api.onchange('margin_percent')
+    def _onchage_margin_percent(self):
+        for line in self:
+            if line.margin_percent == 1 and line.purchase_price != 0:
+                raise UserError(
+                    self.env._("If the cost is not 0, it is not possible to set the margin to 100%")
+                )
+            discount = (1 - line.discount / 100) if line.discount else 1
+            if line.purchase_price != 0:
+                line.price_unit = (line.purchase_price) / (1 - line.margin_percent) / discount
+                tax_included_prices = line.tax_ids.filtered(lambda tax: tax.price_include)
+                for tax in tax_included_prices:
+                    line.price_unit *= 1 + tax.amount / 100
+            line_difference = line.price_unit - line.purchase_price
+            if line.qty_delivered and not line.product_uom_qty:
+                line.margin = line_difference * line.qty_delivered
+            else:
+                line.margin = line_difference * line.product_uom_qty
