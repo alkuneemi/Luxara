e9ea7500c7ae5ecd884413c6f1db5c5b06c07568
divy-odoo <divy@odoo.com>
2026-03-02 15:13:16 +0530

    [IMP] website: add contains conditional visiblity for record field
    Previously, only "Equal to" and "Is not equal to" conditions were
    available when defining conditional visibility rules on record
    fields. This prevented users from matching multiple records using
    partial values.
    
    This commit adds support for selecting multiple values by
    introducing comparator,
    - "Contains": show the field if any selected condition matches.
    - "Doesn't contain": show the field if none of the selected
      conditions match.
    
    task-4128359

--- addons/website/static/tests/tours/website_form_editor.js
+++ addons/website/static/tests/tours/website_form_editor.js
@@ -1030,7 +1030,7 @@ registerWebsitePreviewTour(
         },
         {
             content: "Open condition comparator select",
-            trigger: "[data-container-title='Field'] #hidden_condition_record_opt",
+            trigger: "[data-container-title='Field'] #checkbox_condition_record_opt",
             run: "click",
         },
         {
