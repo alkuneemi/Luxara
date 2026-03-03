ef1606e146273e997e084f29a60da07cd1721200
shmn-odoo <shmn@odoo.com>
2026-03-03 13:00:36 +0530

    [FIX] hr, *: prioritize employee data linked to users
    *: hr_holidays,test_discuss_full
    
    Steps to reproduce,
    Create an employee linked to a user
    Archive the employee and remove the link to the user
    Create another employee for the same user
    Go to Discuss > 'General' channel
    Open the member list and click on the user's avatar
    
    Before this commit,
    Clicking on the avatar opened a popover showing outdated information from the
    archived employee record instead of the new one.
    
    Cause,
    By default, the server sends employee data ordered by name. Since both records
    have the same name, the order is non-deterministic. The client then attempts to
    match the employee's company to the current user's company, falling back to the
    first record in the list if no match is found.
    
    Fix,
    Filter out archived records (treating them as non-existent) first, then sort the
    remaining employee records to prioritize records linked to users, falling back
    to descending order of creation for identical results before attempting to find
    a match.
    
    After this commit,
    Clicking on the avatar shows the correct employee details in the popover.
    
    task-6175765

--- addons/hr/static/tests/web/m2x_avatar_user.test.js
+++ addons/hr/static/tests/web/m2x_avatar_user.test.js
@@ -1,7 +1,8 @@
 import { describe, expect, test } from "@odoo/hoot";
-import { queryAllTexts } from "@odoo/hoot-dom";
+import { queryAllTexts, waitFor } from "@odoo/hoot-dom";
 import { contains, makeMockServer, mountView } from "@web/../tests/web_test_helpers";
-import { contains as mailContains } from "@mail/../tests/mail_test_helpers";
+import { user } from "@web/core/user";
+import { click as mailClick, contains as mailContains } from "@mail/../tests/mail_test_helpers";
 import { defineHrModels } from "@hr/../tests/hr_test_helpers";
 
 describe.current.tags("desktop");
@@ -128,4 +129,58 @@ test("avatar card preview with hr (partner_id field)", async () => {
     ]);
     await contains(".o_action_manager:eq(0)").click();
     await mailContains(".o_avatar_card", { count: 0 });
+});
+
+test("employees with user prioritized over employees without user", async () => {
+    const { env } = await makeMockServer();
+    const partnerId = env["res.partner"].create({ name: "John" });
+    const userId = env["res.users"].create({ partner_id: partnerId });
+    const otherCompanyId = env["res.company"].create({ name: "Other Company" });
+    const [department1Id, department2Id] = env["hr.department"].create([
+        { name: "RD Discuss" },
+        { name: "RD Livechat" },
+    ]);
+    env["hr.employee"].create([
+        {
+            department_id: department1Id,
+            work_contact_id: partnerId,
+            user_id: userId,
+            company_id: user.activeCompany.id,
+        },
+        {
+            department_id: department2Id,
+            work_contact_id: partnerId,
+            user_id: userId,
+            company_id: otherCompanyId,
+        },
+        {
+            department_id: department2Id,
+            work_contact_id: partnerId,
+            company_id: user.activeCompany.id,
+        },
+        {
+            department_id: department2Id,
+            work_contact_id: partnerId,
+            company_id: user.activeCompany.id,
+            active: false,
+        },
+    ]);
+    env["m2x.avatar.user"].create({ partner_id: partnerId });
+    await mountView({
+        type: "kanban",
+        resModel: "m2x.avatar.user",
+        arch: `<kanban>
+            <templates>
+                <t t-name="card">
+                    <field name="partner_id" widget="many2one_avatar_user"/>
+                </t>
+            </templates>
+        </kanban>`,
+    });
+    await mailClick(".o_m2o_avatar > img");
+    await waitFor(".o-mail-avatar-card-name");
+    await mailContains(".o_card_user_infos > span:contains('John')");
+    await mailContains(".o_card_user_infos > span:contains('RD Discuss')");
+    await mailClick(".o_action_manager:eq(0)");
+    await waitFor(".o_avatar_card");
 });
