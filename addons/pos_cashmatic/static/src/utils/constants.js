8d425ec6157a15d91ec3cad9e8abfdbed1eab63d
Loukas Wets (lowe) <lowe@odoo.com>
2026-03-09 14:38:03 +0100

    [ADD] pos_cashmatic: support for cash machine
    Cashmatic is a company providing self-service cash
    machines, which can automatically count cash and
    dispense change. This commit adds basic support
    for Cashmatic cash machines via an HTTP REST API
    connection.
    
    Task-5887125

---
+++ addons/pos_cashmatic/static/src/utils/constants.js
@@ -0,0 +1,9 @@
+export const POST_REQUESTS = {
+    login: "/api/user/Login",
+    renewToken: "/api/user/RenewToken",
+    startPayment: "/api/transaction/StartPayment",
+    cancelPayment: "/api/transaction/CancelPayment",
+    activeTransaction: "/api/device/ActiveTransaction",
+    lastTransaction: "/api/device/LastTransaction",
+    startWithdrawal: "/api/transaction/StartWithdrawal",
+};
