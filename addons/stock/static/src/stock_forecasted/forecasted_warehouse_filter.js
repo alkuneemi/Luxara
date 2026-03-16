87324f9148baf8b4b4cf5f40459c9e85a2958141
jodh-odoo <jodh@odoo.com>
2026-03-16 18:46:38 +0530

    [IMP] stock: support all warehouses in forecast report
    Purpose:
    ========
    In multi-warehouse environments, The forecasted report is currently
    limited to a single warehouse. users must switch between warehouses
    to understand stock availability, making it difficult to get a
    complete and reliable overview.
    
    This limitation reduces efficiency and makes planning more complex,
    especially when stock is distributed across multiple warehouses.
    
    With This Commit:
    =================
    The forecasted report now supports working across multiple warehouses,
    allowing users to get a consolidated view of stock.
    
    - Introduces an `All Warehouses` option in the report,
    - Ensures data is properly aggregated across warehouses,
    - Keeps the report consistent and usable when All Warehouses
      are selected.
    
    This improves visibility of stock and simplifies analysis for
    multi-warehouse setups.
    
    task-4385275

--- addons/stock/static/src/stock_forecasted/forecasted_warehouse_filter.js
+++ addons/stock/static/src/stock_forecasted/forecasted_warehouse_filter.js
@@ -1,7 +1,6 @@
 import { Dropdown } from "@web/core/dropdown/dropdown";
 import { DropdownItem } from "@web/core/dropdown/dropdown_item";
-import { useService } from "@web/core/utils/hooks";
-import { Component, onWillStart } from "@odoo/owl";
+import { Component } from "@odoo/owl";
 
 export class ForecastedWarehouseFilter extends Component {
     static template = "stock.ForecastedWarehouseFilter";
@@ -9,14 +8,8 @@ export class ForecastedWarehouseFilter extends Component {
     static props = { action: Object, setWarehouseInContext: Function, warehouses: Array };
 
     setup() {
-        this.orm = useService("orm");
         this.context = this.props.action.context;
         this.warehouses = this.props.warehouses;
-        onWillStart(this.onWillStart)
-    }
-
-    async onWillStart() {
-        this.displayWarehouseFilter = (this.warehouses.length > 1);
     }
 
     _onSelected(id){
--- addons/stock/static/src/stock_forecasted/stock_forecasted.js
+++ addons/stock/static/src/stock_forecasted/stock_forecasted.js
@@ -4,6 +4,7 @@ import { _t } from "@web/core/l10n/translation";
 import { registry } from "@web/core/registry";
 import { View } from "@web/views/view";
 import { ControlPanel } from "@web/search/control_panel/control_panel";
+import { user } from "@web/core/user";
 
 import { ForecastedButtons } from "./forecasted_buttons";
 import { ForecastedDetails } from "./forecasted_details";
@@ -40,14 +41,15 @@ export class StockForecasted extends Component {
         onWillStart(this._getReportValues);
     }
 
+    get warehouseId() {
+        return this.context.warehouse_id;
+    }
+
     async _getReportValues() {
         await this._getResModel();
         const isTemplate = !this.resModel || this.resModel === 'product.template';
         this.reportModelName = `stock.forecasted_product_${isTemplate ? "template" : "product"}`;
-        this.warehouses.splice(0, this.warehouses.length);
-        this.warehouses.push(...await this.orm.searchRead('stock.warehouse', [],['id', 'name', 'code']));
-        if (!this.context.warehouse_id) {
-            this.updateWarehouse(this.warehouses[0].id);
+        await this._loadWarehouses();
         }
         const reportValues = await this.orm.call(this.reportModelName, "get_report_values", [], {
             context: this.context,
@@ -85,8 +87,25 @@ export class StockForecasted extends Component {
         }
     }
 
+    async _loadWarehouses() {
+        const warehouses = await this.orm.searchRead(
+            "stock.warehouse",
+            [["company_id", "=", user.activeCompany.id]],
+            ["id", "name", "code"]
+        );
+        this.warehouses =
+            warehouses.length > 1
+                ? [{ id: 0, name: _t("All Warehouses") }, ...warehouses]
+                : warehouses;
+
+        // If no warehouse is selected by the user, set a default.
+        if (this.warehouseId === undefined) {
+            this.updateWarehouse(this.warehouses[0].id);
+        }
+    }
+
     async updateWarehouse(id) {
-        const hasPreviousValue = this.context.warehouse_id !== undefined;
+        const hasPreviousValue = this.warehouseId !== undefined;
         this.context.warehouse_id = id;
         if (hasPreviousValue) {
             await this.reloadReport();
@@ -105,10 +124,19 @@ export class StockForecasted extends Component {
         return this.action.doAction(actionRequest, options);
     }
 
+    get selectedWarehouseIds() {
+        if (this.warehouseId === 0) {
+            return this.warehouses
+                .filter((warehouse) => warehouse.id > 0)
+                .map((warehouse) => warehouse.id);
+        }
+        return [this.warehouseId];
+    }
+
     get graphDomain() {
         const domain = [
             ["state", "=", "forecast"],
-            ["warehouse_id", "=", this.context.warehouse_id],
+            ["warehouse_id", "in", this.selectedWarehouseIds],
         ];
         if (this.resModel === "product.template") {
             domain.push(["product_tmpl_id", "=", this.productId]);
