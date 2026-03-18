41586f638c9a3c4fe1eff8daa8dc4971600e59be
Rahul Prajapati <rapr@odoo.com>
2026-03-18 16:02:10 +0530

    [FIX] web: prevent duplicate IDs in hoot mock models
    Previously, new record IDs were generated using the maximum existing ID.
    If the last record was deleted, newly created records could reuse the same ID.
    
    This change stores the last generated ID on the model and uses it to
    generate new records, ensuring uniqueness.
    
    closes odoo/odoo#250661
    
    Signed-off-by: Julien Mougenot (jum) <jum@odoo.com>

--- addons/web/static/tests/_framework/mock_server/mock_model.js
+++ addons/web/static/tests/_framework/mock_server/mock_model.js
@@ -1565,6 +1565,7 @@ export class Model extends Array {
     _filters = [];
     /** @type {string | null} */
     _inherit = null;
+    _lastRecId = 0;
     /** @type {string} */
     _name = "";
     /** @type {Record<string, (record: ModelRecord) => any>} */
@@ -3009,7 +3010,7 @@ export class Model extends Array {
      * @private
      */
     _getNextId() {
-        return Math.max(0, ...this.map((record) => record?.id || 0)) + 1;
+        return ++this._lastRecId;
     }
 
     /**
