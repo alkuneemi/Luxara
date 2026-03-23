af23967594dcc3c9988bf62dcba4eae8fe256e08
Robin Lejeune (role) <role@odoo.com>
2026-03-23 10:02:44 +0100

    [IMP] html_builder, *: allow translation of image src
    *: base, website
    
    This commit allows to translate an image's src, making it possible to
    display a different image in language A and language B.
    
    Those translations should not be exported through .pot files, as they
    are purely user-dependent (they are in fact not real translations,
    replacing an image by another one is different from translating "hello
    world"). That is why we don't modify `translate.py` in tools, but
    instead take advantage of the `.translate` directive added in qweb
    templates with commit [c391c08].
    
    The system works thus as such:
    - Handle whatever modification on the builder side (in JS)
    - On save, add `.translate` on the src attribute (in JS), so that the
    arch view is saved with the directive
    - Go through the qweb rendering, in charge of calling the translation
    layer as it parses `src.translate`
    - Remove the `.translate` suffix to generate valid HTML on the client
    
    In order to avoid having src URLs wrapped inside a translating <span>
    (see `translate_func()`), which sends wrong requests when the DOM is
    loaded, we watch the actual translatable URL string on a
    `data-oe-translate-src` attribute.
    
    The same approach is applied on html fields as on arch views.
    
    On the JS side, only the `src` and `srcset` are actually translatable.
    This means that other attributes on the image element are not synced on
    translated pages: everything related to the original ID, mimetype, etc.
    still refer to the image from the main language.
    
    It is not an issue for now, as other image options are not available in
    translation (shape, quality, filter, etc.), we just copy what was
    applied in the main language. It is planned to add those options in
    translation too later - but it will need a custom approach, because all
    these pieces of information are technical and must thus absolutely be
    "translated" as a whole, with the rest of the context and linked to the
    new image src. They cannot be "translated" naively string per string.
    
    [c391c08]: https://github.com/odoo/odoo/commit/c391c08813a1a2fe38abd328cb0e05700e73b323
    
    task-3626918

--- addons/html_builder/static/src/plugins/image/image_tool_option_plugin.js
+++ addons/html_builder/static/src/plugins/image/image_tool_option_plugin.js
@@ -17,6 +17,10 @@ import { isImageSupportedForProcessing } from "@html_editor/main/media/image_pos
 
 const IMAGE_LINK_ALIGN_CLASSES = ["mx-auto", "ms-auto", "me-auto"];
 
+/**
+ * @typedef {(() => boolean)[]} should_optimize_image_predicates
+ */
+
 export class ImageToolOptionPlugin extends Plugin {
     static id = "imageToolOption";
     static dependencies = [
@@ -40,6 +44,7 @@ export class ImageToolOptionPlugin extends Plugin {
             SetNewWindowAction,
             AltAction,
         },
+        should_optimize_image_predicates: () => true, // default
         on_will_save_media_dialog_handlers: this.onWillSaveMediaDialogHandlers.bind(this),
         normalize_processors: this.migrateImages.bind(this),
     };
@@ -80,22 +85,25 @@ export class ImageToolOptionPlugin extends Plugin {
                 if (!imgInfo.originalSrc || !imgInfo.originalId) {
                     continue;
                 }
-                const isImgSupportedForProcessing = await isImageSupportedForProcessing(
-                    image,
-                    await getMimetype(image, imgInfo)
-                );
                 const newDataset = {};
-                if (isImgSupportedForProcessing) {
-                    newDataset.formatMimetype = this.config.defaultImageMimetype ?? "image/webp";
-                    const original = await loadImage(imgInfo.originalSrc);
-                    const maxWidth = image.dataset.width
-                        ? image.naturalWidth
-                        : original.naturalWidth;
-                    const optimizedWidth = Math.min(
-                        maxWidth,
-                        computeMaxDisplayWidth(node || this.editable)
+                if (this.checkPredicates("should_optimize_image_predicates")) {
+                    const isImgSupportedForProcessing = await isImageSupportedForProcessing(
+                        image,
+                        await getMimetype(image, imgInfo)
                     );
-                    newDataset.resizeWidth = optimizedWidth;
+                    const newDataset = {};
+                    if (isImgSupportedForProcessing) {
+                        newDataset.formatMimetype = this.config.defaultImageMimetype ?? "image/webp";
+                        const original = await loadImage(imgInfo.originalSrc);
+                        const maxWidth = image.dataset.width
+                            ? image.naturalWidth
+                            : original.naturalWidth;
+                        const optimizedWidth = Math.min(
+                            maxWidth,
+                            computeMaxDisplayWidth(node || this.editable)
+                        );
+                        newDataset.resizeWidth = optimizedWidth;
+                    }
                 }
                 const updateImageAttributes = await this.dependencies.imagePostProcess.processImage(
                     {
@@ -279,3 +287,4 @@ export class AltAction extends BuilderAction {
 }
 
 registry.category("builder-plugins").add(ImageToolOptionPlugin.id, ImageToolOptionPlugin);
+registry.category("translation-plugins").add(ImageToolOptionPlugin.id, ImageToolOptionPlugin);
