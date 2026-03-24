e968196c04f19daf6a23053e11062c5a8f3b6608
tshe-odoo <tshe@odoo.com>
2026-03-24 15:02:39 +0530

    [IMP] hr_holidays, _*: update Time Off Type view layout and behavior
    _* - hr_work_entry
    
    - update Time Off Type list view columns order and visibility
    - Keep the value of 'Allocation Approval' as 'None needed' when 'Requires Allocation' is unchecked.
    
    task-5441115

--- addons/hr_holidays/models/hr_work_entry_type.py
+++ addons/hr_holidays/models/hr_work_entry_type.py
@@ -77,6 +77,7 @@ class HrWorkEntryType(models.Model):
         ('hr', 'By Time Off Officer'),
         ('manager', "By Employee's Approver"),
         ('both', "By Employee's Approver and Time Off Officer")], default='hr', string='Approval',
+        compute='_compute_allocation_validation_type', readonly=False, store=True,
         tracking=True,
         help="""Select the level of approval needed in case of request by employee
             #     - No validation needed: The employee's request is automatically approved.
@@ -122,6 +123,14 @@ class HrWorkEntryType(models.Model):
         'The maximum excess amount should be greater than 0. If you want to set 0, disable the negative cap instead.'
     )
     time_off_selectable = fields.Boolean(string="Selectable in Time Off", default=True, tracking=True)
+
+    @api.depends('requires_allocation')
+    def _compute_allocation_validation_type(self):
+        for record in self:
+            if not record.requires_allocation:
+                record.allocation_validation_type = 'no_validation'
+            else:
+                record.allocation_validation_type = 'hr'
 
     @api.model
     def _search_valid(self, operator, value):
