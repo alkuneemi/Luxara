4daf9351777fd4a95b684a4a2bd4ca2b08c42ab0
mano-odoo <mano@odoo.com>
2026-03-24 16:46:43 +0100

    [FIX] html_editor,htm_builder: allow real gradient transparency
    When a gradient color with some transparency is applied through the
    color picker, it applies a `background-image()` but doesn't override the
    background color.
    
    In cases of element which has the o_cc(x) classes, this means that the
    gradient is not transparent because the o_cc background color is
    displayed underneath.
    
    task-6046066

--- addons/html_editor/static/tests/color_selector.test.js
+++ addons/html_editor/static/tests/color_selector.test.js
@@ -628,7 +628,7 @@ test("should be able to show preview when hovering radial type button", async ()
     const gradientAfter = `radial-gradient(circle farthest-side at 25% 25%, rgb(223, 124, 196) 0%, rgb(108, 53, 130) 100%)`;
 
     const { el } = await setupEditor(
-        `<p>a<font style="background-image: ${gradientBefore};">[bcd]</font>e</p>`
+        `<p>a<font style="background-image: ${gradientBefore}; background-color: transparent;">[bcd]</font>e</p>`
     );
     await expandToolbar();
     await click(".o-we-toolbar .o-select-color-background");
@@ -647,7 +647,7 @@ test("should be able to show preview when hovering radial type button", async ()
     await hover("button[title='Extend to the farthest side']");
     await animationFrame();
     expect(getContent(el)).toBe(
-        `<p>a<font style="background-image: ${gradientAfter};">[bcd]</font>e</p>`
+        `<p>a<font style="background-image: ${gradientAfter}; background-color: transparent;">[bcd]</font>e</p>`
     );
     expect("button[title='Extend to the farthest side']").not.toHaveClass("active");
 
@@ -656,7 +656,7 @@ test("should be able to show preview when hovering radial type button", async ()
     await animationFrame();
     expect(".o_custom_gradient_button").toHaveStyle({ backgroundImage: gradientBefore });
     expect(getContent(el)).toBe(
-        `<p>a<font style="background-image: ${gradientBefore};">[bcd]</font>e</p>`
+        `<p>a<font style="background-image: ${gradientBefore}; background-color: transparent;">[bcd]</font>e</p>`
     );
     expect("button[title='Extend to the farthest side']").not.toHaveClass("active");
 
@@ -670,7 +670,7 @@ test("should be able to show preview when hovering radial type button", async ()
 
     expect(".o_custom_gradient_button").toHaveStyle({ backgroundImage: gradientAfter });
     expect(getContent(el)).toBe(
-        `<p>a<font style="background-image: ${gradientAfter};">[bcd]</font>e</p>`
+        `<p>a<font style="background-image: ${gradientAfter}; background-color: transparent;">[bcd]</font>e</p>`
     );
     expect("button[title='Extend to the farthest side']").toHaveClass("active");
 });
