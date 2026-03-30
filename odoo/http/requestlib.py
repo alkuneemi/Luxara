d3ce727b1fb973d6038880d0a106eacce1432f80
Krzysztof Magusiak (krma) <krma@odoo.com>
2026-03-30 08:23:57 +0000

    [REF] http: remove most of end of file imports
    Move imports to the top of the file when possible. Most circular imports
    were introduced because of the global `request`, moving it into the
    `__init__.py` solves most of the issues.

--- odoo/http/requestlib.py
+++ odoo/http/requestlib.py
@@ -19,30 +19,29 @@ from werkzeug.datastructures import (
     MultiDict,
 )
 from werkzeug.exceptions import NotFound
-from werkzeug.local import LocalStack
 from werkzeug.urls import URL, url_encode, url_parse
 from werkzeug.utils import redirect
 
 from odoo.tools import consteq, json_default
 
+from . import request, _request_stack
+from .dispatcher import HttpDispatcher
+from .geoip import GeoIP
+from .response import FutureResponse, Response
+from .session import DEFAULT_LANG, STORED_SESSION_BYTES, get_default_session
+
 if typing.TYPE_CHECKING:
     from collections.abc import Iterable, Mapping
-
-    import werkzeug.routing
 
     from odoo.api import Environment
     from odoo.models import BaseModel
     from odoo.modules.registry import Registry
 
-    from .response import Response
     from .routing_map import Endpoint
 
     HeaderType = Mapping[str, str | Iterable[str]] | Iterable[tuple[str, str]]
 
 _logger = logging.getLogger('odoo.http')
-
-_request_stack = LocalStack()
-request: Request = _request_stack()  # type: ignore[assignment]
 
 CSRF_TOKEN_SALT = 60 * 60 * 24 * 365  # 1 year
 """ The default csrf token lifetime, a salt against BREACH. """
@@ -364,13 +363,9 @@ class Request:
         self.httprequest = httprequest
 
 
-# ruff: noqa: E402
 if typing.TYPE_CHECKING:
+    import werkzeug.wrappers
     HTTPRequest = werkzeug.wrappers.Request
     from ._facade import DEFAULT_MAX_CONTENT_LENGTH
 else:
     from ._facade import DEFAULT_MAX_CONTENT_LENGTH, HTTPRequest  # noqa: F401
-from .dispatcher import HttpDispatcher
-from .geoip import GeoIP
-from .response import FutureResponse, Response
-from .session import DEFAULT_LANG, STORED_SESSION_BYTES, get_default_session
