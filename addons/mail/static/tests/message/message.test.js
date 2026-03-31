2e89f530702acdf791a54b564a25f938b3b3ad3f
akus-odoo <akus@odoo.com>
2026-03-31 18:05:06 +0530

    [IMP] mail: enable full composer while editing chatter messages
    Users could not open the full composer when editing messages or notes
    from the Chatter, limiting editing capabilities.
    
    This change allows opening the full composer in edit mode, reusing the
    existing message data and updating the original message instead of
    creating a new one. It also ensures proper synchronization between
    inline and full composer states.
    
    Task - 6015654

--- addons/mail/static/tests/message/message.test.js
+++ addons/mail/static/tests/message/message.test.js
@@ -12,6 +12,7 @@ import {
     hover,
     insertText,
     listenStoreFetch,
+    mailModels,
     onRpcBefore,
     openDiscuss,
     openFormView,
@@ -53,6 +54,7 @@ const { DateTime } = luxon;
 
 describe.current.tags("desktop");
 defineMailModels();
+mailModels.MailComposeMessage._views = {};
 
 test("Start edition on click edit", async () => {
     const pyEnv = await startServer();
@@ -340,6 +342,52 @@ test("Can edit message comment in chatter (MacOS)", async () => {
     await canEditMessageCommentInChatter({ isMacOS: true });
 });
 
+test("edit message via full composer should update message", async () => {
+    const pyEnv = await startServer();
+    const partnerId = pyEnv["res.partner"].create({ name: "TestPartner" });
+    pyEnv["mail.message"].create({
+        author_id: serverState.partnerId,
+        body: "original message",
+        message_type: "comment",
+        model: "res.partner",
+        res_id: partnerId,
+    });
+    mockService("action", {
+        doAction(action) {
+            if (action.res_model === "mail.compose.message") {
+                expect(action.context.default_res_ids).toEqual([partnerId]);
+                action.context.default_res_ids = JSON.stringify(action.context.default_res_ids);
+            }
+            return super.doAction(...arguments);
+        },
+    });
+    mailModels.MailComposeMessage._views = {
+        "form,false": `
+            <form>
+                <field name="body" widget="html_composer_message"/>
+            </form>
+        `,
+    };
+    await start();
+    await openFormView("res.partner", partnerId);
+    await click(".o-mail-Message [title='Edit']");
+    await contains(".o-mail-Composer");
+    await click(".o-mail-Composer button[title='More Actions']");
+    await click(".dropdown-item:contains('Open Full Composer')");
+
+    const paragraph = await waitFor(".modal .odoo-editor-editable .o-paragraph");
+    setSelection({
+        anchorNode: paragraph.firstChild,
+        anchorOffset: paragraph.textContent.length,
+    });
+    await contains(".modal .o_form_view");
+    const editable = document.querySelector(".modal .odoo-editor-editable");
+    const editor = { document, editable };
+    await htmlInsertText(editor, " updated");
+    await click(".modal .o_form_button_save");
+    await contains(".o-mail-Message-content:text('original message updated')");
+});
+
 test("Basic list of edit message actions in chatter", async () => {
     const pyEnv = await startServer();
     const partnerId = pyEnv["res.partner"].create({ name: "TestPartner" });
@@ -355,8 +403,9 @@ test("Basic list of edit message actions in chatter", async () => {
     await click(".o-mail-Message [title='Edit']");
     await contains(".o-mail-Message .o-mail-Composer.o-focused");
     await click(".o-mail-Message .o-mail-Composer button[title='More Actions']");
-    await contains(".dropdown-menu .dropdown-item", { count: 1 });
+    await contains(".dropdown-menu .dropdown-item", { count: 2 });
     await contains(".dropdown-menu .dropdown-item:has(:text('Attach Files'))");
+    await contains(".dropdown-menu .dropdown-item:has(:text('Open Full Composer'))");
 });
 
 test("Cursor is at end of composer input on edit", async () => {
--- addons/mail/static/tests/mock_server/mock_models/mail_composer_message.js
+++ addons/mail/static/tests/mock_server/mock_models/mail_composer_message.js
@@ -1,5 +1,36 @@
+import { mailDataHelpers } from "@mail/../tests/mock_server/mail_mock_server";
+
 import { models } from "@web/../tests/web_test_helpers";
 
 export class MailComposeMessage extends models.ServerModel {
     _name = "mail.compose.message";
+
+    web_save(ids, values, kwargs = {}) {
+        const context = kwargs.context || {};
+        if (!context.is_editing_message || !context.default_message_id) {
+            return super.web_save(ids, values, kwargs);
+        }
+        const messageId = context.default_message_id;
+        const MailMessage = this.env["mail.message"];
+        const [message] = MailMessage.browse(messageId);
+        if (!message) {
+            return [];
+        }
+        const msg_values = {};
+        if (values.body !== null) {
+            msg_values.body = values.body || "";
+        }
+        MailMessage.write([messageId], msg_values);
+        this.env["bus.bus"]._sendone(
+            MailMessage._bus_notification_target(message.id),
+            "mail.record/insert",
+            new mailDataHelpers.Store(MailMessage.browse(message.id)).get_result()
+        );
+        return [
+            {
+                id: messageId,
+                body: values.body || "",
+            },
+        ];
+    }
 }
