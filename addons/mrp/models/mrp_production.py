cb88e2cf41d1c317ecb0cf402bbb60f225f92a71
joseph <josv@odoo.com>
2026-04-03 17:09:21 +0200

    [FIX] mrp: prevent mo fadeout without lot on component
    For a MO with component tracked by lot is possible to close it without assigning the lot number even when they are marked for manual consumption. From the shopfloor this will close the MO that will dissapear when it's gone it will throw an user error, without the MO reappearing.
    
    **Steps to reproduce**
    
    * Create a product "final"
    * Create a product "component" tracked by lot
    * Update the on hand quantity and don't assign a lot number
    * Create a BoM for "final" with "component" as component and manual comsumption
    * Create a MO and confirm it
    * Go to shopfloor
    * Set "final" quantity
    * Set "component" quantity
    * Close the MO
    -> The MO disappear (leaving a user error)
    -> Since the "component" does not have a lot number it should not disappear
    
    **Observation**
    
    When clicking on "Closing production" it will call [onClickValidateButton](https://github.com/odoo/enterprise/blob/37b21f93828fae2caec87cfd8413e144751eefc0/mrp_workorder/static/src/mrp_display/mrp_display_record.js#L481-L482)() that will lead us to validate,
    and validate will [call](https://github.com/odoo/enterprise/blob/37b21f93828fae2caec87cfd8413e144751eefc0/mrp_workorder/static/src/mrp_display/mrp_display_record.js#L511) the function [pre_button_mark_done](https://github.com/odoo/enterprise/blob/37b21f93828fae2caec87cfd8413e144751eefc0/mrp_workorder/static/src/mrp_display/mrp_display_record.js#L507) in python.
    
    in pre_button_mark_done, we will skip any check on the lot number, since we already have set qty_producing (when setting "final" quantity), it will avoid _set_quantities where a check for lot number is made: https://github.com/odoo/odoo/blob/9218d302b0fd56c1854d95d3756c0f5e9c9c8700/addons/mrp/models/mrp_production.py#L2235-L2239
    https://github.com/odoo/odoo/blob/9218d302b0fd56c1854d95d3756c0f5e9c9c8700/addons/mrp/models/mrp_production.py#L2839-L2844
    And it will also skip the consumption wizard since it doesn't check for lot number nor allow to set lot numbers:
    https://github.com/odoo/odoo/blob/35f804995118e6ffe150c9c7a7bad4844bd0e0ed/addons/mrp/models/mrp_production.py#L1658-L1660
    After avoiding both checks we go back to validate.
    
    In validate and we update the variable underValidation which will [trigger the fadeout animation](https://github.com/odoo/enterprise/blob/37b21f93828fae2caec87cfd8413e144751eefc0/mrp_workorder/static/src/mrp_display/mrp_display_record.js#L154)
    When the [animation ends](https://github.com/odoo/enterprise/blob/37b21f93828fae2caec87cfd8413e144751eefc0/mrp_workorder/static/src/mrp_display/mrp_display_record.js#L662-L665), the call to realValidation will be triggered and will call productionValidation that will send a call to [button_mark_done](https://github.com/odoo/enterprise/blob/37b21f93828fae2caec87cfd8413e144751eefc0/mrp_workorder/static/src/mrp_display/mrp_display_record.js#L551)
    
    In button_mark_done, when calling _action_done on the move_lines, we will finally check that there is a lot number, which will trigger an error :
    https://github.com/odoo/odoo/blob/35f804995118e6ffe150c9c7a7bad4844bd0e0ed/addons/stock/models/stock_move_line.py#L664-L669
    but because the fadeout animation is already over, the mo will have disappeared, which means we can't correct the userError that has been raised.
    
    opw-6060310

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -2247,6 +2247,7 @@ class MrpProduction(models.Model):
 
         for production in self.env['mrp.production'].browse(productions_auto):
             production._set_quantities()
+        self._check_missing_lot_id_in_raw_move()
 
         consumption_issues = self._get_consumption_issues()
         if consumption_issues:
@@ -2823,7 +2824,6 @@ class MrpProduction(models.Model):
 
     def _set_quantities(self):
         self.ensure_one()
-        missing_lot_id_products = ""
         if self.product_tracking in ('lot', 'serial') and not self.lot_producing_id:
             self.action_generate_serial()
         if self.product_tracking == 'serial' and float_compare(self.qty_producing, 1, precision_rounding=self.product_uom_id.rounding) == 1:
@@ -2832,12 +2832,14 @@ class MrpProduction(models.Model):
             self.qty_producing = self.product_qty - self.qty_produced
         self._set_qty_producing()
 
+    def _check_missing_lot_id_in_raw_move(self):
+        missing_lot_id_products = ""
         for move in self.move_raw_ids:
             if move.state in ('done', 'cancel') or not move.product_uom_qty:
                 continue
             rounding = move.product_uom.rounding
             if move.manual_consumption:
-                if move.has_tracking in ('serial', 'lot') and (not move.picked or any(not line.lot_id for line in move.move_line_ids if line.quantity and line.picked)):
+                if move.has_tracking in ('serial', 'lot') and (not move.picked or any(not line.lot_id for line in move.move_line_ids if not float_is_zero(line.quantity, precision_rounding=rounding) and line.picked)):
                     missing_lot_id_products += "\n  - %s" % move.product_id.display_name
         if missing_lot_id_products:
             error_msg = _(
