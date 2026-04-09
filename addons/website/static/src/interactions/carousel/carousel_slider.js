d1139255712349781f282d5de8d67497a72d221b
Robin Lejeune (role) <role@odoo.com>
2026-04-09 10:54:39 +0200

    [IMP] website: support home/end keys on carousels
    When the focus is in a carousel, pressing the "home" key displays the
    first slide and pressing the "end" key displays the last slide.
    
    task-5470023

--- addons/website/static/src/interactions/carousel/carousel_slider.js
+++ addons/website/static/src/interactions/carousel/carousel_slider.js
@@ -10,6 +10,14 @@ export class CarouselSlider extends Interaction {
             "t-on-slid.bs.carousel": this.onSlidCarousel,
             "t-on-focusin": () => window.Carousel.getInstance(this.el)?.pause(),
             "t-on-focusout": this.resumeCarouselCycling,
+            "t-on-keydown": (ev) => {
+                const hotkey = getActiveHotkey(ev);
+                if (["home", "end"].includes(hotkey)) {
+                    const childToFocus = hotkey === "home" ? ":first-child" : ":last-child";
+                    ev.preventDefault();
+                    this.el.querySelector(`.carousel-indicators > ${childToFocus}`).click();
+                }
+            },
         },
         "img": {
             "t-on-load": this.computeMaxHeight,
