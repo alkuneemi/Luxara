02084f57f52d39e40fc6d3450bb958a257cf785c
nsirjacobs <nosir@odoo.com>
2026-04-13 16:45:49 +0200

    [FIX] hr_skills_event: ensure new onsite events match view domain
    Onsite events created from the "Onsite" view or the employee resume selector do not appear immediatlely after creation
    
    This occurs because currently the domain for onsite events requires that the event to have multiple slots
    as well as to have at least one employee registered to it.
    Therefore, newly created records often fail these criteria and remain hidden.
    
    In further versions, this pr: https://github.com/odoo/odoo/pull/246285/
    changes the domain of the event selector in the employee resume by removing the dependency on the multiple slots and filtering
    by the specific employee for registration.
    
    This change is not stable to backport as it indroduces the `employee_id` field as an invisible field in the xml to be able to compare in the domain.
    
    This commit partly changes both domains to not require the multiple slots anymore, while still showing all events for which an employee is registered.
    This commit also ensures that when an event is created from the Onsite view or selector, the current user's employee will be registered to it.
    
    Steps to reproduce
    - Go to employees->Learning->Onsite
    - Select New and create an event
    - Go back to Onsite Courses
    - You will not see the created event (unless it is multi_slot and an employee was registered)
    
    opw-5915686

--- addons/hr_skills_event/models/hr_resume_line.py
+++ addons/hr_skills_event/models/hr_resume_line.py
@@ -9,7 +9,8 @@ class HrResumeLine(models.Model):
     event_id = fields.Many2one(
         'event.event', string="Onsite Course", compute='_compute_event_id',
         store=True, readonly=True, index='btree_not_null',
-        domain="[('is_multi_slots', '=', True), ('registration_ids', 'any', [('partner_id.employee', '=', True)])]"
+        domain="[('registration_ids', 'any', [('partner_id.employee', '=', True)])]",
+        context={'is_onsite': True},
     )
     course_type = fields.Selection(
         selection_add=[('onsite', 'Onsite')],
