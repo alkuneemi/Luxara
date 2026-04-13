555bf92831ce23369ee97a3221ab9bf814ebdfb8
alan-odoo <alan@odoo.com>
2026-04-13 10:06:20 +0200

    test standardization

--- addons/calendar/static/src/views/list_view/calendar_list_controller.js
+++ addons/calendar/static/src/views/list_view/calendar_list_controller.js
@@ -68,7 +68,7 @@ export class CaledarListController extends ListController {
             user.partnerId,
             selectedRecords.map((record) => record.resId),
         ]);
-        if (selectedRecords.length == 1 && user.userId === record.data.user_id.id) {
+        if (selectedRecords.length == 1 && user.userId === selectedRecords[0].data.user_id.id) {
             console.log("is passing to delete only one record")
             const record = selectedRecords[0];
             const partnerIds = record.data.partner_ids.resIds;
@@ -107,5 +107,36 @@ export class CaledarListController extends ListController {
                 this.orm.call("calendar.event", "action_unlink_events", [deletedEventIds]);
             }
         }
+    }
+
+    async deleteRecords(records) {
+        const userAttendeesDetails = await this.orm.call("res.partner", "get_attendee_detail", [
+            user.partnerId,
+            selectedRecords.map((record) => record.resId),
+        ]);
+        const declinedAttendeeIds = [];
+        const deletedEventIds = [];
+        for (const userAttendeeDetails of userAttendeesDetails) {
+            if (userAttendeeDetails.is_organizer) {
+                deletedEventIds.push(userAttendeeDetails.event_id);
+            } else {
+                declinedAttendeeIds.push(userAttendeeDetails.attendee_id);
+            }
+        }
+        if (declinedAttendeeIds){
+            this.orm.call("calendar.attendee", "do_decline", [declinedAttendeeIds]);
+        }
+        if (deletedEventIds){
+            this.orm.call("calendar.event", "action_unlink_events", [deletedEventIds]);
+        }
+    }
+
+    deleteConfirmationDialogProps(records) {
+        return {
+            ...super.deleteConfirmationDialogProps(records),
+            body: "This is a body test",
+            confirm: deleteRecords,
+            confirmLabel: "This is a great test"
+        };
     }
 }
