585fcbebc79027adfd1c47c30621f5c0958cec0c
Sébastien (blse) <blse@odoo.com>
2026-04-13 16:11:37 +0200

    [REF] website: use `.o_conditional_hidden` only inside website builder
    The class `.o_conditional_hidden` was used for two purposes:
    - hide a conditionally visible element when the user clicks on the
      corresponding entry in the "Invisible Elements" panel inside the
      website builder
    - hide all the conditionally visible elements at the beginning when the
      user visits the page, before the style sheet with the css rules for
      the conditionally visible elements is constructed and loaded
    
    This commit introduces a new class `o_conditional_visibility_ready` used
    to control the visibility of those elements while the style sheet is not
    ready yet. The class `o_conditional_hidden` is now only used while the
    website builder is opened.
    
    task-5149984

--- addons/website/static/src/builder/plugins/website_visibility_plugin.js
+++ addons/website/static/src/builder/plugins/website_visibility_plugin.js
@@ -25,11 +25,6 @@ export class WebsiteVisibilityPlugin extends Plugin {
                 toggle: (el, show) => el.classList.toggle("o_conditional_hidden", !show),
             },
         ],
-        on_snippet_dropped_handlers: ({ snippetEl }) => {
-            for (const el of selectElements(snippetEl, "[data-visibility=conditional]")) {
-                el.classList.remove("o_conditional_hidden");
-            }
-        },
         normalize_processors: (root) => {
             const allowedDeviceOverride = this.config.isMobileView(this.editable)
                 ? ".o_snippet_mobile_invisible"
@@ -51,10 +46,8 @@ export class WebsiteVisibilityPlugin extends Plugin {
             for (const el of selectElements(root, ".o_snippet_override_invisible")) {
                 el.classList.remove("o_snippet_override_invisible");
             }
-            for (const el of selectElements(root, "[data-visibility=conditional]")) {
-                // we add `o_conditional_hidden` in the saved version,
-                // and it will be removed on load in `unhideConditionalElements`
-                el.classList.add("o_conditional_hidden");
+            for (const el of selectElements(root, ".o_conditional_hidden")) {
+                el.classList.remove("o_conditional_hidden");
             }
         },
         on_mobile_view_switched_handlers: () => {
@@ -68,6 +61,9 @@ export class WebsiteVisibilityPlugin extends Plugin {
                 }
                 this.dependencies.visibility.invalidateVisibility();
             }
+        },
+        snippet_preview_dialog_iframe_processors: ({ iframe }) => {
+            iframe.contentDocument.body.classList.add("o_conditional_visibility_ready");
         },
     };
 
