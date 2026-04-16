826e98552de93f9b2c0087dc22b5a43c6e7dd6f8
joseph <josv@odoo.com>
2026-04-16 10:34:11 +0200

    [FIX] purchase_stock: only count incoming delivery in po qty received
    On a Purchase Order, if you duplicate the delivery, change its Operation Type and validate it, it will still count towards the received quantity on the po.
    
    Steps to reproduce:
    -------------------
    * Create a PO and confirm it
    * Open the receipt, validate and duplicate it
    * Go on the duplicated receipt and change its Operation Type
    * Confirm the duplicated receipt.
    -> the Received Qty on the Po counted the duplicated and modified transfer.
    
    Observation:
    -------------
    When we duplicate a stock.picking, we will call ```copy``` and copy all its ```move_ids``` (stock.move), and in stock.move we will copy the ```purchase_line_id``` : https://github.com/odoo/odoo/blob/754898c58fded0e3c3c30702f15258f1dadcfa6c/odoo/models.py#L5868 https://github.com/odoo/odoo/blob/754898c58fded0e3c3c30702f15258f1dadcfa6c/odoo/models.py#L5881-L5892
    https://github.com/odoo/odoo/blob/3e6d93bef49bb2718e8aa1756f95c2a6117c8d55/addons/purchase_stock/models/stock_move.py#L13-L15
    the ```purchase_line_id``` will keep the link with the pol.
    The fields are copied since they have ```copy=True``` https://github.com/odoo/odoo/blob/754898c58fded0e3c3c30702f15258f1dadcfa6c/addons/stock/models/stock_picking.py#L614 https://github.com/odoo/odoo/blob/754898c58fded0e3c3c30702f15258f1dadcfa6c/odoo/fields.py#L304
    
    Because new stock moves are linked directly to the Purchase Order Line (POL), the qty_received calculation aggregates all associated move quantities. Currently, the logic fails to filter by picking type, meaning it may incorrectly include moves that do not correspond to picking type of the purchase order in the total received quantity.
    https://github.com/odoo/odoo/blob/cfc63060926db4cec773c159b8ecf97dc0b36d1a/addons/purchase_stock/models/purchase_order_line.py#L73-L76
    opw-6088239

--- addons/purchase_stock/models/purchase_order_line.py
+++ addons/purchase_stock/models/purchase_order_line.py
@@ -70,7 +70,7 @@ class PurchaseOrderLine(models.Model):
                             # receive the product physically in our stock. To avoid counting the
                             # quantity twice, we do nothing.
                             pass
-                        elif move.origin_returned_move_id and move.origin_returned_move_id._is_purchase_return() and not move.to_refund:
+                        elif move.origin_returned_move_id and move.origin_returned_move_id._is_purchase_return() and not move.to_refund or move.picking_type_id != move.purchase_line_id.order_id.picking_type_id:
                             pass
                         else:
                             total += move.product_uom._compute_quantity(move.quantity, line.product_uom, rounding_method='HALF-UP')
