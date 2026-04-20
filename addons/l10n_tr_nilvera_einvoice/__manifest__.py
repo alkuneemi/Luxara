odoo/odoo

[ADD] l10n_tr_nilvera_einvoice: support commercial invoices/bills
Extends Nilvera integration to support processing of commercial
invoices and bills (ProfileID=TICARIFATURA) in alignment with
Nilvera’s API requirements.

This implementation enables full lifecycle handling of commercial
documents, including status tracking, response management, and
synchronization with Nilvera.

Supported scenarios:
  - ProfileID: TICARIFATURA
  - InvoiceTypeCode: SATIS, TEVKIFAT, ISTISNA, IHRACKAYITLI
  - Move types: sale and purchase

Behavior note:
  - If InvoiceTypeCode is different from the supported values,
    it falls back to SATIS to ensure bill response handling
    remains functional.

Unsupported cases:
  - Credit notes

Key features added:
  - Creation of commercial invoices/bills with ProfileID TICARIFATURA.
  - Import adjustments to support TICARIFATURA-specific flows.
  - Status tracking for commercial invoices, with UI banner showing:
      * Acceptance
      * Rejection (including rejection reason)
      * Automatic responses from Nilvera.
  - Automatic retrieval of updated PDF report upon rejection.
  - Ability to accept or reject commercial bills from Odoo.
  - Scheduled job (12-hour interval) to periodically synchronize
    response status with Nilvera.

Technical notes:
  - Enhances existing Nilvera invoice import logic to differentiate
    commercial scenarios.
  - Introduces scheduled synchronization mechanism for response updates.

task-4741223

addons/l10n_tr_nilvera_einvoice/__manifest__.py
@@ -27,6 +27,7 @@ Features include:
         'views/account_tax_views.xml',
         'views/product_views.xml',
         'views/res_config_settings_views.xml',
+        'wizards/l10n_tr_nilvera_einvoice_ticafatura_response_wizard_views.xml',
     ],
     'demo': [
         'demo/demo_company.xml',

alkuneemi/Luxara

      * Rejection (including rejection reason on chatter)

addons/l10n_tr_nilvera_einvoice/__manifest__.py
@@ -27,6 +27,7 @@ Features include:
         'views/account_tax_views.xml',
         'views/product_views.xml',
         'views/res_config_settings_views.xml',
+        'wizards/l10n_tr_nilvera_einvoice_ticarifatura_response_wizard_views.xml',
     ],
     'demo': [
         'demo/demo_company.xml',