9f7d2ea73f64c7b6120028e7c866a1536358d8cf
Julien Coppin (jucop) <jucop@odoo.com>
2026-04-20 16:15:35 +0200

    [IMP] web: improve datetime picker navigation and controls
    * Click/touch on date(time) field: open picker
    * Reach date(time) field with TAB: focus content
    * On focus:
    	- SPACE to open the picker
    	- TAB to reach the elements: arrows, month, selected date, time, buttons
    	- On elements: ENTER and SPACE to act
    	- On mini-calendar: keyboard arrows to move around the days/months/years
    	- ESC to close the picker
    * Add a prop to hide weeknumbers
    * Show seconds displays correctly
    * Display "Clear" icon and "Apply" button
    
    task-5149047

--- addons/web/static/tests/core/domain_selector/domain_selector.test.js
+++ addons/web/static/tests/core/domain_selector/domain_selector.test.js
@@ -195,7 +195,7 @@ test("building a domain with a datetime", async () => {
     // Change the date in the datepicker
     await contains(".o_datetime_input").click();
     await contains(getPickerCell("26", true)).click();
-    await press("enter");
+    await press("control+enter");
     await animationFrame();
 
     // The input field should display the date and time in the user's timezone
@@ -2635,7 +2635,7 @@ test(`datetime: "in range" operator`, async () => {
 
     await contains(".o_datetime_input:last").click();
     await contains(getPickerCell("26", true)).click();
-    await press("enter");
+    await press("control+enter");
     await animationFrame();
     expect.verifySteps([
         `["&", ("datetime", ">=", "2023-04-20 00:00:00"), ("datetime", "<=", "2023-04-26 23:59:59")]`,
@@ -2722,7 +2722,7 @@ test(`date: "in range" operator`, async () => {
 
     await contains(".o_datetime_input:last").click();
     await contains(getPickerCell("26", true)).click();
-    await press("enter");
+    await press("control+enter");
     await animationFrame();
     expect.verifySteps([`["&", ("date", ">=", "2023-04-20"), ("date", "<=", "2023-04-26")]`]);
 
