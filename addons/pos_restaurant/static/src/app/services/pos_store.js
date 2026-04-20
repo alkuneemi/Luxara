7bac4b30db2fa05abf5fbf37ca5c03daed651118
rpka-odoo <rpka@odoo.com>
2026-04-20 16:38:27 +0530

    [IMP] point_of_sale: improve receipt and prep ticket UI
    In this commit -
    Enhanced font styling for receipt and preparation ticket
    Added floor information next to table number on preparation ticket
    
    Task - 6125322

--- addons/pos_restaurant/static/src/app/services/pos_store.js
+++ addons/pos_restaurant/static/src/app/services/pos_store.js
@@ -1055,6 +1055,7 @@ patch(PosStore.prototype, {
     getOrderData(order, reprint) {
         return {
             ...super.getOrderData(order, reprint),
+            floor_name: order.table_id?.floor_id?.name || "",
             customer_count: order.getCustomerCount(),
         };
     },
