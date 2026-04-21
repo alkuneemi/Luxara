2dfabe1ed1e2e764e4b8396530167010d0f04d61
Aaron Bohy <aab@odoo.com>
2026-04-21 13:21:52 +0200

    [TOREMOVE] selection autosave kanban - waiting for ouelh PR

--- addons/web/static/src/views/fields/selection/selection_field.js
+++ addons/web/static/src/views/fields/selection/selection_field.js
@@ -146,7 +146,7 @@ export const selectionField = {
     isEmpty: (record, fieldName) => record.data[fieldName] === false,
     extractProps({ viewType, placeholder }, dynamicInfo) {
         const props = {
-            autosave: viewType === "kanban",
+            autosave: viewType === "card",
             placeholder,
             required: dynamicInfo.required,
             domain: dynamicInfo.domain,
