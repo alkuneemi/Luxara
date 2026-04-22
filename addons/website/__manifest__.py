d594f5a59cceeb2ec2cc753c0498d67a5d58c53f
Chrysanthe (chgo) <chgo@odoo.com>
2026-04-22 16:04:12 +0200

    [IMP] website: introduce `s_duo_images` snippet

--- addons/website/__manifest__.py
+++ addons/website/__manifest__.py
@@ -173,6 +173,7 @@
         'views/snippets/s_features_bento.xml',
         'views/snippets/s_projects_grid.xml',
         'views/snippets/s_banner_contained.xml',
+        'views/snippets/s_duo_images.xml',
         'views/snippets/s_dynamic_snippet.xml',
         'views/snippets/s_dynamic_snippet_carousel.xml',
         'views/snippets/s_banner_product.xml',
