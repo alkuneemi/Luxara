b68fde32916309d2f3c5ec8b9ffd2083abb1dde9
Christophe Matthieu <chm@odoo.com>
2026-04-23 13:58:36 +0200

    [REF] website: replace request.website with context-based website_id
    The use of `request.website` introduces a dependency on global state, and
    `get_current_website` introduces an implicit fallback logic, that
    complicates the ORM's behavior in non-HTTP contexts (CRONs, CLI, RPC).
    
    This commit establishes the foundation for a strictly context-driven
    website resolution:
    - Contextualization in `ir.http`: The `website_id` is now explicitly
    injected into the context during the `ir.http` routing process.
    - Deprioritizing `request`: Replaced direct `request.website` access with
    `env.context.get('website_id')`.
    - Compatibility: Use the `self.env.website` depending of the context.
    - Explicit JS Passing: Updated several frontend calls to explicitly pass
    the `website_id` in the RPC context, reducing reliance on server-side
    fallback logic.
    
    Future Goal: Remove the `host_id` entirely. This will ensure that any
    website-dependent logic fails fast if the context is not explicitly
    defined, leading to more predictable and robust code.
    
    Task 6037834

--- addons/website_sale/controllers/wishlist.py
+++ addons/website_sale/controllers/wishlist.py
@@ -12,7 +12,8 @@ class ProductWishlist(Controller):
         price = product._get_combination_info_variant()["price"]
 
         Wishlist = request.env["product.wishlist"]
-        if request.website.is_public_user():
+        website = request.env["website"].get_current_website()
+        if website.is_public_user():
             Wishlist = Wishlist.sudo()
             partner_id = False
         else:
@@ -20,8 +21,8 @@ class ProductWishlist(Controller):
 
         wish = Wishlist._add_to_wishlist(
             request.pricelist.id,
-            request.website.currency_id.id,
-            request.website.id,
+            website.currency_id.id,
+            website.id,
             price,
             product_id,
             partner_id,
@@ -44,7 +45,8 @@ class ProductWishlist(Controller):
     @route("/shop/wishlist/remove/<int:wish_id>", type="jsonrpc", auth="public", website=True)
     def remove_from_wishlist(self, wish_id, **_kw):
         wish = request.env["product.wishlist"].browse(wish_id)
-        if request.website.is_public_user():
+        website = request.env["website"].get_current_website()
+        if website.is_public_user():
             wish_ids = request.session.get("wishlist_ids") or []
             if wish_id in wish_ids:
                 request.session["wishlist_ids"].remove(wish_id)
