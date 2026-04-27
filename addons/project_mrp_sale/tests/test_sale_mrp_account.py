fab1078f4dbf177fb0f8f34cbd761fb215ade252
Ester Andreetto <esand@odoo.com>
2026-04-27 15:45:04 +0200

    [FIX] sale_project_stock: allow project population in MO (2 steps delivery)
    Issue before this commit:
    When confirming a Sale Order with multi-step delivery routes (Pick +
    Deliver), the generated Manufacturing Order did not inherit the
    project_id from the Sale Order, even though the project was correctly
    set on the SO.
    
    Steps to reproduce the issue:
    1. Download Inventory, Sales, PLM, Project
    2. Enable multi-step routes in settings and unarchive MTO from routes
    3. Go to Products and modify
    	1. Furniture Assembly inserting Project in "Create on Order" a
        ttribute and a test project template
    	2. Drawer selecting MTO and Manifacture in Inventory tab
    4. Create a new quotation inserting 2 products: Furniture Assembly and
    Drawer and confirm it
    5. Go to "Manufacturing" smart button
    6. See in Miscellaneous tab the Project is setted as default
    7. Go to warehouses, select the WH of your company and select Pick then
    Deliver (2 steps) as Outgoing Shipments
    8. Go to Routes, select Deliver in two steps (pick + ship) and change
    "Destination location" of "Pull From"  from Parteners/Customers into
    WH/output
    9. Go to Rules:
    	1. "Pull From" (the one with Partener/Customers as destination
        location and MTO as route): change "Destination location" of from
        Parteners/Customers into WH/output
    	2. "Push To" (the one with Partener/Customers as destination
        location and with the deliver in 2 steps as route): change action
        from "Push To" to "Pull from"
    10. Create a new quotation inserting 2 products: Furniture Assembly
    and Drawer and confirm it
    5. Go to "Manufacturing" smart button
    6. See in Miscellaneous tab the Project is not setted
    
    Cause of the issue:
    https://github.com/odoo/odoo/blob/d1955028bb95eff8d33c1c2b1c211d8520bb33a2/addons/sale_project_stock/models/stock_move.py#L71-L76
    This function retrieves the project only based on the sale_line_id,
    which is correctly populated in a one-step delivery because the
    procurement is directly linked to the Sales Order (SO).
    However, the sale_line_id is not propagated to procurements that are not
    directly linked to the SO.
    In this scenario, with a two-step delivery, there are two stock moves:
    from Stock to WH/Output, and from WH/Output to the Customer. Only the
    second move (WH/Output > Customer) is linked to the SO, while this
    information is not propagated to the first move. As a result, the
    project_id information is lost.
    
    Reason to introduce the fix:
    Be sure that the project is automatically setted in the MO when a SO is
    confirmed whatever the intial delivery configuration is.
    
    opw-5913988

--- addons/project_mrp_sale/tests/test_sale_mrp_account.py
+++ addons/project_mrp_sale/tests/test_sale_mrp_account.py
@@ -15,3 +15,33 @@ class TestSaleMrpAccount(TestMultistepManufacturing):
         self.assertFalse(self.sale_order.mrp_production_ids.project_id)
         self.sale_order.action_confirm()
         self.assertEqual(self.sale_order.mrp_production_ids.project_id, project)
+
+    def test_mo_get_project_from_so_in_2_steps_delivery(self):
+        """ Ensure the project of MO is inherited from the SO in a 2-step delivery (Pick + Ship). """
+        self.warehouse.delivery_steps = 'pick_ship'
+
+        # Change Routes and Rules to have the following configuration:
+        #    - Step 1: Test/Stock (location_src_id) > Test/Output (location_dest_id)
+        #    - Step 2: Test/Output (location_src_id) > Parteners/Customers (location_dest_id)
+        output_loc = self.warehouse.delivery_route_id.rule_ids.filtered(lambda r: r.action == 'push').location_src_id
+        self.warehouse.delivery_route_id.rule_ids.filtered(lambda r: r.action == 'pull').write({'location_dest_id': output_loc.id})
+        if self.warehouse.mto_pull_id.location_dest_id == self.env.ref('stock.stock_location_customers'):
+            self.warehouse.mto_pull_id.write({'location_dest_id': output_loc.id})
+        self.warehouse.delivery_route_id.rule_ids.filtered(lambda r: r.action == 'push').write({'action': 'pull'})
+
+        product_service = self.env['product.product'].create({
+            'name': 'Furniture Assembly',
+            'type': 'service',
+            'service_tracking': 'project_only',
+        })
+
+        self.env['sale.order.line'].create({
+            'order_id': self.sale_order.id,
+            'name': product_service.name,
+            'product_id': product_service.id,
+            'product_uom_qty': 1.0,
+            'price_unit': 50.0,
+        })
+
+        self.sale_order.action_confirm()
+        self.assertTrue(self.sale_order.mrp_production_ids.project_id)
