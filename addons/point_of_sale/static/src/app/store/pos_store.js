odoo/odoo

[FIX] point_of_sale, pos_*: ensure logo is displayed on receipt
 pos_*  : pos_self_order
Some clients reported that the company logo was not displayed on receipts.
This issue was reproducible when using iOS with a large logo file, and in some cases when the browser cache was disabled.
This commit addresses the issue by:
- Using the image_256 version of the company logo to fix rendering on iOS.
- Storing the logo as data url to avoid repeated URL requests and cache-related issues.

Task-5055910

addons/point_of_sale/static/src/app/services/pos_store.js
@@ -379,6 +379,7 @@ export class PosStore extends WithLazyGetterTrap {
         });
 
         await this.processProductAttributes();
+        await this.config.cacheReceiptLogo();
     }
     cashMove() {
         this.hardwareProxy.openCashbox(_t("Cash in / out"));

alkuneemi/Luxara

addons/point_of_sale/static/src/app/store/pos_store.js
@@ -360,6 +360,7 @@ export class PosStore extends Reactive {
         }
         this.computeProductPricelistCache();
         await this.processProductAttributes();
+        await this.company.cacheReceiptLogo();
     }
     cashMove() {
         this.hardwareProxy.openCashbox(_t("Cash in / out"));