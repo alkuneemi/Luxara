MoLeap/odooo

[IMP] html_editor: automatically expand toolbar in mobile
This commit always keeps the toolbar in mobile expanded.
It also adds a fading effect at the end of the mobile toolbar to help
discoverability.

task-6009844

addons/html_editor/static/tests/format/font.test.js
@@ -16,15 +16,18 @@ test("should change the containing block with the font", async () => {
     expect(getContent(el)).toBe("<blockquote>ab[cde]fg</blockquote>");
 });
 
-test("should have font tool only if the block is content editable", async () => {
-    for (const [contenteditable, count] of [
-        [false, 0],
-        [true, 1],
-    ]) {
-        await setupEditor(
-            `<div contenteditable="${contenteditable}"><p><span contenteditable="true">ab[cde]fg</span></p></div>`
-        );
-        await expandToolbar();
-        expect(".btn[name='font']").toHaveCount(count);
-    }
+test("should have font tool only if the block is content editable (non editable)", async () => {
+    await setupEditor(
+        `<div contenteditable="false"><p><span contenteditable="true">ab[cde]fg</span></p></div>`
+    );
+    await expandToolbar();
+    expect(".btn[name='font']").toHaveCount(0);
+});
+
+test("should have font tool only if the block is content editable (editable)", async () => {
+    await setupEditor(
+        `<div contenteditable="true"><p><span contenteditable="true">ab[cde]fg</span></p></div>`
+    );
+    await expandToolbar();
+    expect(".btn[name='font']").toHaveCount(1);
 });

alkuneemi/Luxara

addons/html_editor/static/tests/format/font.test.js
@@ -16,17 +16,20 @@ test("should change the containing block with the font", async () => {
     expect(getContent(el)).toBe("<blockquote>ab[cde]fg</blockquote>");
 });
 
-test("should have font tool only if the block is content editable", async () => {
-    for (const [contenteditable, count] of [
-        [false, 0],
-        [true, 1],
-    ]) {
-        await setupEditor(
-            `<div contenteditable="${contenteditable}"><p><span contenteditable="true">ab[cde]fg</span></p></div>`
-        );
-        await expandToolbar();
-        expect(".btn[name='font_type']").toHaveCount(count);
-    }
+test("should have font tool only if the block is content editable (non editable)", async () => {
+    await setupEditor(
+        `<div contenteditable="false"><p><span contenteditable="true">ab[cde]fg</span></p></div>`
+    );
+    await expandToolbar();
+    expect(".btn[name='font_type']").toHaveCount(0);
+});
+
+test("should have font tool only if the block is content editable (editable)", async () => {
+    await setupEditor(
+        `<div contenteditable="true"><p><span contenteditable="true">ab[cde]fg</span></p></div>`
+    );
+    await expandToolbar();
+    expect(".btn[name='font_type']").toHaveCount(1);
 });
 
 test("Should show the default font display name", async () => {