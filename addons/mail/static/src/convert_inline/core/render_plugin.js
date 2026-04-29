835e24361b9ded885e6b4d7ddff28206fd69ba99
abd-msyukyu-odoo <abd@odoo.com>
2026-04-29 17:16:59 +0200

    truc

--- addons/mail/static/src/convert_inline/core/render_plugin.js
+++ addons/mail/static/src/convert_inline/core/render_plugin.js
@@ -14,20 +14,37 @@ export class RenderPlugin extends Plugin {
         if (!analysisTree) {
             return;
         }
+        // case study: background color + color filter => should apply the same logic as a normal
+        // filter? => if the logic is to create a new attachment. If it uses browser rendering
+        // capabilities, then it won't work
+
+        // honestly what's best for me right now:
+        // add template wrapper capabilities to tif-telse mso nodes and render a custom mso template
+        // have a table replaces div template for all cases where mso requires a table instead of a div
+        // I don't know any other cases right now
+        // ideally, facts already migrated on the correct node, allowing to apply the table or the conditional
+        // mso stuff directly on that node, and we should have all facts necessary to render that node as
+        // its alternative way by MSO => I can do it by setting a subtree option, and every subnode has MSO options
+        // if necessary in this case
+        // I think right now the analysis parsing solves most issues for me
+        // => about tables
+        // normally, a table will ask that its direct container is not a table => if it is, we create a row + td to wrap
+        // it => becomes legal again
+
         // Currently, all my nodes have annotations/facts that concern THEM that were either:
         // -identified
         // -propagated from a descendant or an ancestor
         // these facts should be used to alter what the node itself becomes.
-        // these facts could result in the node becoming more than one node.
-            // isn't there an issue that these new nodes would require a new analysis phase?
-        // 
-
-
+        // eg a container becomes a table or has to be wrapped in one
+        // a node could need an alternative MSO representation
+        // idea:
+        // Create a new tree where each node can have an associated rendering template
+        // -> button node can become => MSO comment (if mso / MSO BUTTON / endif / if !mso) buttonNode w. model + endif mso
 
         // pass 4 / B):
         // iterate over "identity" => identity should be a model which
         // is comprised of every potential part (this is where isolation of concerns ends?)
-        
+
         // per node, allocate a model, related to a template
         // the template has slots, and aggregates all concerns (can be done with xpath if we want to isolate some stuff)
         // complete the model so that every slot has its complete info
