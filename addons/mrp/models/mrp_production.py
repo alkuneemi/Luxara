b70c95c613b123ec9a0e294b350a09069af6a200
japat-odoo <japat@odoo.com>
2026-04-29 12:35:16 +0530

    [IMP] mrp: preserve earliest scheduled and deadline dates on MO merge
    When merging manufacturing orders, use the earliest scheduled date and the
    earliest deadline from selected MOs for the merged MO instead of overriding
    them with the merge date.
    
    TaskID-6169235

--- addons/mrp/models/mrp_production.py
+++ addons/mrp/models/mrp_production.py
@@ -2629,6 +2629,8 @@ class MrpProduction(models.Model):
             'uom_id': product_id.uom_id.id,
             'location_final_id': all(mo.location_final_id for mo in self) and len(self.location_final_id) == 1 and self.location_final_id.id,
             'user_id': user_id.id,
+            'date_start': min(self.mapped('date_start')),
+            'date_deadline': min(filter(None, self.mapped('date_deadline')), default=False),
             'reference_ids': [Command.link(r.id) for r in self.reference_ids],
             'origin': ",".join(sorted([production.name for production in self])),
         })
