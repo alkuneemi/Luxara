odoo/odoo

[IMP] spreadsheet: add computedMeasure to odooList

addons/spreadsheet/static/src/list/list_functions.js
@@ -1,5 +1,6 @@
 import { _t } from "@web/core/l10n/translation";
 import { helpers, registries, EvaluationError } from "@odoo/o-spreadsheet";
+import { addListDependencies } from "./list_helpers";
 
 const { arg, toString, toNumber } = helpers;
 const { functionRegistry } = registries;
@@ -32,6 +33,12 @@ const ODOO_LIST_VALUE = {
             return new EvaluationError(_t("The field name should not be empty."));
         }
         assertListsExists(id, this.getters);
+        const column = this.getters
+            .getListDefinition(id)
+            .columns.find((col) => col.name === fieldName);
+        if (column) {
+            addListDependencies(this, id, [column]);
+        }
         return this.getters.getListCellValueAndFormat(id, position, _fieldName);
     },
 };
@@ -63,6 +70,8 @@ const ODOO_LIST = {
     compute: function (listId, rowCount) {
         const id = toString(listId);
         assertListsExists(id, this.getters);
+        const columns = this.getters.getListDefinition(id).columns;
+        addListDependencies(this, id, columns);
         const _rowCount = rowCount ? toNumber(rowCount, this.locale) : undefined;
         if (_rowCount !== undefined && _rowCount <= 0) {
             return new EvaluationError(

addons/spreadsheet/static/src/list/list_helpers.js
@@ -30,3 +30,22 @@ export function hasListFormula(compiledFormula) {
 export function getFirstListFunction(compiledFormula, getters) {
     return compiledFormula.getFunctionsFromTokens(ALL_LIST_FUNCTIONS, getters)[0];
 }
+
+export function addListDependencies(evalContext, listId, columns) {
+    const dependencies = [];
+    for (const column of columns) {
+        if (column.computedBy) {
+            dependencies.push(
+                ...evalContext.getters.getListCompiledMeasureDependencies(listId, column.name)
+            );
+        }
+    }
+    const originPosition = evalContext.__originCellPosition;
+    if (originPosition && dependencies.length) {
+        // The following line is used to reset the dependencies of the cell, to avoid
+        // keeping dependencies from previous evaluation of the LIST formula (i.e.
+        // in case the reference has been changed).
+        evalContext.updateDependencies?.(originPosition);
+        evalContext.addDependencies?.(originPosition, dependencies);
+    }
+}

addons/spreadsheet/static/src/list/list_presentation.js
@@ -1,0 +1,168 @@
+import * as spreadsheet from "@odoo/o-spreadsheet";
+
+const { isEvaluationError, isMatrix, deepEquals } = spreadsheet.helpers;
+const { NotAvailableError } = spreadsheet;
+
+export class ListPresentationLayer {
+    constructor(getters, listId, definition, dataSource) {
+        this.getters = getters;
+        this.id = listId;
+        this.definition = definition;
+        this.dataSource = dataSource;
+        this.computeCache = {};
+    }
+
+    getListHeaderValue(path) {
+        const columnDef = this.definition.columns.find((col) => col.name === path);
+        return columnDef?.string || this.dataSource.getListHeaderValue(path);
+    }
+
+    getListValuesAndFormats(rowCount) {
+        if (rowCount === undefined) {
+            throw new Error("The number of rows to fetch must be specified");
+        }
+        const columns = this.definition.columns.filter((col) => !col.hidden);
+
+        if (columns.length === 0) {
+            return { value: this.getters.getListDisplayName(this.id) };
+        }
+
+        const computedColumns = columns.filter((col) => !!col.computedBy);
+
+        const computedSymbols = new Set(
+            computedColumns.flatMap((col) => {
+                const formula = this.getters.getListCompiledMeasureFormula(this.id, col.name);
+                return formula.tokens
+                    .filter((token) => token.type === "SYMBOL")
+                    .map((t) => t.value);
+            })
+        );
+
+        const columnToFetch = [];
+        for (const col of this.definition.columns) {
+            if ((!col.hidden || computedSymbols.has(col.string)) && !col.computedBy) {
+                columnToFetch.push(col);
+            }
+        }
+
+        if (columnToFetch.length) {
+            columnToFetch.forEach((col) => this.dataSource.addFieldPathToFetch(col.name));
+            // triggers the fetch of the list values up to `rowCount` to fill the datasource cache (if not already done)
+            this.dataSource.getListCellValue(rowCount, columnToFetch[0]?.name);
+        }
+
+        const numberRecordsToLoad = Math.min(this.dataSource.data.length, rowCount);
+        const valuesAndFormats = [];
+        for (const column of columns) {
+            if (column.hidden) {
+                continue;
+            }
+            const currentColumn = [];
+            currentColumn.push({ value: this.getListHeaderValue(column.name) });
+            for (let position = 0; position < numberRecordsToLoad; position++) {
+                const cellValueAndFormat = this.getListCellValueAndFormat(column, position);
+                currentColumn.push(cellValueAndFormat);
+            }
+            valuesAndFormats.push(currentColumn);
+        }
+        return valuesAndFormats;
+    }
+
+    getListCellValueAndFormat(column, position) {
+        if (column && column.computedBy) {
+            return this.computeCellValue(column, position);
+        }
+        return this._getListCellValueAndFormat(position, column.name);
+    }
+
+    _getListCellValueAndFormat(position, path) {
+        // shortcut to pre-fill the fetch list (spares a round of server call)
+        this.dataSource.addFieldPathToFetch(path);
+        const value = this.dataSource.getListCellValue(position, path);
+        if (typeof value === "object" && isEvaluationError(value.value)) {
+            return value;
+        }
+        const field = this.dataSource.getFieldFromFieldPath(path);
+        const format = this._getListFormat(position, path, field);
+        return { value, format };
+    }
+
+    computeCellValue(column, position) {
+        const cacheKey = `${column.name}_${position}`;
+        if (this.computeCache[cacheKey]) {
+            return this.computeCache[cacheKey];
+        }
+        const formula = this.getters.getListCompiledMeasureFormula(this.id, column.name);
+        const getSymbolValue = (symbol) => {
+            const symbolColumn = this.definition.columns.find((col) => col.string === symbol);
+            if (!symbolColumn) {
+                return new NotAvailableError();
+            } else if (symbolColumn.string === column.string) {
+                return new NotAvailableError(); // TODO switch to Cycle error
+            }
+            return this.getListCellValueAndFormat(symbolColumn, position);
+        };
+        let result = this.getters.evaluateCompiledFormula(
+            column.computedBy.sheetId,
+            formula,
+            getSymbolValue
+        );
+        if (isMatrix(result)) {
+            result = result[0][0];
+        }
+        this.computeCache[cacheKey] = result;
+        return result;
+    }
+
+    _getListFormat(position, path, field) {
+        const locale = this.getters.getLocale();
+        switch (field?.type) {
+            case "integer":
+                return "0";
+            case "float":
+                return "#,##0.00";
+            case "monetary": {
+                const currency = this.getListCurrency(position, path, field.currency_field);
+                if (!currency) {
+                    return "#,##0.00";
+                }
+                return this.getters.computeFormatFromCurrency(currency);
+            }
+            case "date":
+                return locale.dateFormat;
+            case "datetime":
+                return locale.dateFormat + " " + locale.timeFormat;
+            case "char":
+            case "text":
+                return "@";
+            default:
+                return undefined;
+        }
+    }
+
+    getListCurrency(position, path, currentFieldName) {
+        return this.dataSource.getListCurrency(position, path, currentFieldName);
+    }
+
+    refresh() {
+        this.computeCache = {};
+        this.dataSource.load({ reload: true });
+    }
+
+    addDomain(domain) {
+        this.dataSource.addDomain(domain);
+    }
+
+    updateDefinition(nextDefinition) {
+        const currentCompute = this.definition.columns.map((col) => col.computedBy);
+        const nextCompute = nextDefinition.columns.map((col) => col.computedBy);
+        const computeChanged = !deepEquals(currentCompute, nextCompute);
+        this.definition = nextDefinition;
+        this.computeCache = {};
+        this.dataSource.onDefinitionChange(nextDefinition, computeChanged);
+    }
+
+    invalidateCache() {
+        this.computeCache = {};
+    }
+}

addons/spreadsheet/static/src/list/plugins/list_core_plugin.js
@@ -1,13 +1,14 @@
 import { CommandResult } from "../../o_spreadsheet/cancelled_reason";
-import { helpers } from "@odoo/o-spreadsheet";
+import { helpers, CompiledFormula } from "@odoo/o-spreadsheet";
 import { OdooCorePlugin } from "@spreadsheet/plugins";
 
-const { getMaxObjectId, deepEquals, deepCopy } = helpers;
+const { getMaxObjectId, deepEquals, deepCopy, getCanonicalSymbolName } = helpers;
 
 /**
  * @typedef {Object} ListColumn
  * @property {string} name The technical field path of the column
  * @property {string} string The custom display name of the column
+ * @property {{formula: string, sheetId:string}} computedBy The MeasureState id
  * @property {boolean} [hidden] Whether the column is hidden or not
  */
 
@@ -22,6 +23,12 @@ const { getMaxObjectId, deepEquals, deepCopy } = helpers;
  * @property {string} actionXmlId
  */
 
+/**
+ * @typedef {Object} MeasureState
+ * @property {CompiledFormula} formula
+ * @property {Range[]} dependencies
+ */
+
 export class ListCorePlugin extends OdooCorePlugin {
     static getters = /** @type {const} */ ([
         "getListDisplayName",
@@ -30,6 +37,8 @@ export class ListCorePlugin extends OdooCorePlugin {
         "getListName",
         "getNextListId",
         "isExistingList",
+        "getListCompiledMeasureFormula",
+        "getListCompiledMeasureDependencies",
     ]);
     constructor(config) {
         super(config);
@@ -37,6 +46,8 @@ export class ListCorePlugin extends OdooCorePlugin {
         this.nextId = 1;
         /** @type {Object.<string, ListDefinition>} */
         this.lists = {};
+        /** @type {Object.<string, Object.<string, MeasureState>>} */
+        this.compiledMeasureFormulas = {};
     }
 
     /**
@@ -130,6 +141,7 @@ export class ListCorePlugin extends OdooCorePlugin {
             }
             case "UPDATE_ODOO_LIST": {
                 this.history.update("lists", cmd.listId, cmd.list);
+                this._compileCalculatedMeasures(cmd.listId, cmd.list);
                 break;
             }
         }
@@ -192,13 +204,85 @@ export class ListCorePlugin extends OdooCorePlugin {
         return id in this.lists;
     }
 
+    getListCompiledMeasureFormula(listId, measureName) {
+        return this.compiledMeasureFormulas[listId]?.[measureName]?.formula;
+    }
+
+    getListCompiledMeasureDependencies(listId, measureName) {
+        return this.compiledMeasureFormulas[listId]?.[measureName]?.dependencies;
+    }
+
     // ---------------------------------------------------------------------
     // Private
     // ---------------------------------------------------------------------
 
     _addList(id, definition) {
         this.history.update("lists", id, definition);
+        this._compileCalculatedMeasures(id, definition);
         this.history.update("nextId", parseInt(id, 10) + 1);
+    }
+
+    _compileCalculatedMeasures(listId, definition) {
+        const computedColumns = definition.columns.filter((col) => col.computedBy);
+        for (const measure of computedColumns) {
+            const compiledFormula = CompiledFormula.Compile(
+                measure.computedBy.formula,
+                measure.computedBy.sheetId,
+                this.getters
+            );
+            this.history.update(
+                "compiledMeasureFormulas",
+                listId,
+                measure.name,
+                "formula",
+                compiledFormula
+            );
+        }
+        for (const measure of computedColumns) {
+            const dependencies = this._computeMeasureFullDependencies(listId, definition, measure);
+            this.history.update(
+                "compiledMeasureFormulas",
+                listId,
+                measure.name,
+                "dependencies",
+                dependencies
+            );
+        }
+    }
+
+    _computeMeasureFullDependencies(listId, definition, measure, exploredMeasures = new Set()) {
+        const rangeDependencies = [];
+        const formula = this.compiledMeasureFormulas[listId]?.[measure.name]?.formula;
+        exploredMeasures.add(measure.name);
+        for (const usedSymbols of formula.symbols) {
+            const otherMeasure = definition.columns.find(
+                (measureCandidate) =>
+                    getCanonicalSymbolName(measureCandidate.name) === usedSymbols &&
+                    measure.name !== measureCandidate.name
+            );
+
+            if (
+                !otherMeasure ||
+                !otherMeasure.computedBy ||
+                exploredMeasures.has(otherMeasure.name)
+            ) {
+                continue;
+            }
+
+            rangeDependencies.push(
+                ...this._computeMeasureFullDependencies(
+                    listId,
+                    definition,
+                    otherMeasure,
+                    exploredMeasures
+                )
+            );
+        }
+        rangeDependencies.push(...formula.rangeDependencies.filter((range) => !range.invalidXc));
+        rangeDependencies.push(
+            ...formula.getNamedRangesInFormula(this.getters).map((namedRange) => namedRange.range)
+        );
+        return rangeDependencies;
     }
 
     /**

alkuneemi/Luxara

addons/spreadsheet/static/src/list/list_functions.js
@@ -1,5 +1,6 @@
 import { _t } from "@web/core/l10n/translation";
 import { helpers, registries, EvaluationError } from "@odoo/o-spreadsheet";
+import { addListDependencies } from "./list_helpers";
 
 const { arg, toString, toNumber } = helpers;
 const { functionRegistry } = registries;
@@ -32,6 +33,12 @@ const ODOO_LIST_VALUE = {
             return new EvaluationError(_t("The field name should not be empty."));
         }
         assertListsExists(id, this.getters);
+        const column = this.getters
+            .getListDefinition(id)
+            .columns.find((col) => col.name === _fieldName);
+        if (column) {
+            addListDependencies(this, id, [column]);
+        }
         return this.getters.getListCellValueAndFormat(id, position, _fieldName);
     },
 };
@@ -63,6 +70,8 @@ const ODOO_LIST = {
     compute: function (listId, rowCount) {
         const id = toString(listId);
         assertListsExists(id, this.getters);
+        const columns = this.getters.getListDefinition(id).columns;
+        addListDependencies(this, id, columns);
         const _rowCount = rowCount ? toNumber(rowCount, this.locale) : undefined;
         if (_rowCount !== undefined && _rowCount <= 0) {
             return new EvaluationError(

addons/spreadsheet/static/src/list/list_helpers.js
@@ -30,3 +30,22 @@ export function hasListFormula(compiledFormula) {
 export function getFirstListFunction(compiledFormula, getters) {
     return compiledFormula.getFunctionsFromTokens(ALL_LIST_FUNCTIONS, getters)[0];
 }
+
+export function addListDependencies(evalContext, listId, columns) {
+    const dependencies = [];
+    for (const column of columns) {
+        if (column.computedBy) {
+            dependencies.push(
+                ...evalContext.getters.getListCompiledColumnDependencies(listId, column.name)
+            );
+        }
+    }
+    const originPosition = evalContext.__originCellPosition;
+    if (originPosition && dependencies.length) {
+        // The following line is used to reset the dependencies of the cell, to avoid
+        // keeping dependencies from previous evaluation of the LIST formula (i.e.
+        // in case the reference has been changed).
+        evalContext.updateDependencies?.(originPosition);
+        evalContext.addDependencies?.(originPosition, dependencies);
+    }
+}

addons/spreadsheet/static/src/list/list_presentation.js
@@ -1,0 +1,164 @@
+import * as spreadsheet from "@odoo/o-spreadsheet";
+
+const { isEvaluationError, isMatrix } = spreadsheet.helpers;
+const { NotAvailableError, CircularDependencyError } = spreadsheet;
+
+export class ListPresentationLayer {
+    constructor(getters, listId, definition, dataSource) {
+        this.getters = getters;
+        this.id = listId;
+        this.definition = definition;
+        this.dataSource = dataSource;
+        this.computeCache = {};
+    }
+
+    getListHeaderValue(path) {
+        const columnDef = this.definition.columns.find((col) => col.name === path);
+        return columnDef?.string || this.dataSource.getListHeaderValue(path);
+    }
+
+    getListValuesAndFormats(rowCount) {
+        if (rowCount === undefined) {
+            throw new Error("The number of rows to fetch must be specified");
+        }
+        const columns = this.definition.columns.filter((col) => !col.hidden);
+
+        if (columns.length === 0) {
+            return { value: this.getters.getListDisplayName(this.id) };
+        }
+
+        const computedColumns = columns.filter((col) => !!col.computedBy);
+
+        const computedSymbols = new Set(
+            computedColumns.flatMap((col) => {
+                const formula = this.getters.getListCompiledColumnFormula(this.id, col.name);
+                return formula.tokens
+                    .filter((token) => token.type === "SYMBOL")
+                    .map((t) => t.value);
+            })
+        );
+
+        const columnToFetch = [];
+        for (const col of this.definition.columns) {
+            if ((!col.hidden || computedSymbols.has(col.string)) && !col.computedBy) {
+                columnToFetch.push(col);
+            }
+        }
+
+        if (columnToFetch.length) {
+            columnToFetch.forEach((col) => this.dataSource.addFieldPathToFetch(col.name));
+            // triggers the fetch of the list values up to `rowCount` to fill the datasource cache (if not already done)
+            this.dataSource.getListCellValue(rowCount, columnToFetch[0]?.name);
+        }
+
+        const numberRecordsToLoad = Math.min(this.dataSource.data.length, rowCount);
+        const valuesAndFormats = [];
+        for (const column of columns) {
+            if (column.hidden) {
+                continue;
+            }
+            const currentColumn = [];
+            currentColumn.push({ value: this.getListHeaderValue(column.name) });
+            for (let position = 0; position < numberRecordsToLoad; position++) {
+                const cellValueAndFormat = this.getListCellValueAndFormat(column, position);
+                currentColumn.push(cellValueAndFormat);
+            }
+            valuesAndFormats.push(currentColumn);
+        }
+        return valuesAndFormats;
+    }
+
+    getListCellValueAndFormat(column, position) {
+        if (column && column.computedBy) {
+            return this.computeCellValue(column, position);
+        }
+        return this._getListCellValueAndFormat(position, column.name);
+    }
+
+    _getListCellValueAndFormat(position, path) {
+        // shortcut to pre-fill the fetch list (spares a round of server call)
+        this.dataSource.addFieldPathToFetch(path);
+        const value = this.dataSource.getListCellValue(position, path);
+        if (typeof value === "object" && isEvaluationError(value.value)) {
+            return value;
+        }
+        const field = this.dataSource.getFieldFromFieldPath(path);
+        const format = this._getListFormat(position, path, field);
+        return { value, format };
+    }
+
+    computeCellValue(column, position) {
+        const cacheKey = `${column.name}_${position}`;
+        if (this.computeCache[cacheKey]) {
+            return this.computeCache[cacheKey];
+        }
+        const formula = this.getters.getListCompiledColumnFormula(this.id, column.name);
+        const getSymbolValue = (symbol) => {
+            const symbolColumn = this.definition.columns.find((col) => col.string === symbol);
+            if (!symbolColumn) {
+                return new NotAvailableError();
+            } else if (symbolColumn.string === column.string) {
+                return new CircularDependencyError();
+            }
+            return this.getListCellValueAndFormat(symbolColumn, position);
+        };
+        let result = this.getters.evaluateCompiledFormula(
+            column.computedBy.sheetId,
+            formula,
+            getSymbolValue
+        );
+        if (isMatrix(result)) {
+            result = result[0][0];
+        }
+        this.computeCache[cacheKey] = result;
+        return result;
+    }
+
+    _getListFormat(position, path, field) {
+        const locale = this.getters.getLocale();
+        switch (field?.type) {
+            case "integer":
+                return "0";
+            case "float":
+                return "#,##0.00";
+            case "monetary": {
+                const currency = this.getListCurrency(position, path, field.currency_field);
+                if (!currency) {
+                    return "#,##0.00";
+                }
+                return this.getters.computeFormatFromCurrency(currency);
+            }
+            case "date":
+                return locale.dateFormat;
+            case "datetime":
+                return locale.dateFormat + " " + locale.timeFormat;
+            case "char":
+            case "text":
+                return "@";
+            default:
+                return undefined;
+        }
+    }
+
+    getListCurrency(position, path, currentFieldName) {
+        return this.dataSource.getListCurrency(position, path, currentFieldName);
+    }
+
+    refresh() {
+        this.computeCache = {};
+        this.dataSource.load({ reload: true });
+    }
+
+    addDomain(domain) {
+        this.dataSource.addDomain(domain);
+    }
+
+    updateDefinition(nextDefinition) {
+        this.definition = nextDefinition;
+        this.dataSource.onDefinitionChange(nextDefinition);
+    }
+
+    invalidateCache() {
+        this.computeCache = {};
+    }
+}

addons/spreadsheet/static/src/list/plugins/list_core_plugin.js
@@ -1,13 +1,14 @@
 import { CommandResult } from "../../o_spreadsheet/cancelled_reason";
-import { helpers } from "@odoo/o-spreadsheet";
+import { helpers, CompiledFormula } from "@odoo/o-spreadsheet";
 import { OdooCorePlugin } from "@spreadsheet/plugins";
 
-const { getMaxObjectId, deepEquals, deepCopy } = helpers;
+const { getMaxObjectId, deepEquals, deepCopy, getCanonicalSymbolName } = helpers;
 
 /**
  * @typedef {Object} ListColumn
  * @property {string} name The technical field path of the column
  * @property {string} string The custom display name of the column
+ * @property {{formula: string, sheetId:string}} computedBy The formula to compute the column
  * @property {boolean} [hidden] Whether the column is hidden or not
  */
 
@@ -22,6 +23,12 @@ const { getMaxObjectId, deepEquals, deepCopy } = helpers;
  * @property {string} actionXmlId
  */
 
+/**
+ * @typedef {Object} ComputeState
+ * @property {CompiledFormula} formula
+ * @property {Range[]} dependencies
+ */
+
 export class ListCorePlugin extends OdooCorePlugin {
     static getters = /** @type {const} */ ([
         "getListDisplayName",
@@ -30,6 +37,8 @@ export class ListCorePlugin extends OdooCorePlugin {
         "getListName",
         "getNextListId",
         "isExistingList",
+        "getListCompiledColumnFormula",
+        "getListCompiledColumnDependencies",
     ]);
     constructor(config) {
         super(config);
@@ -37,6 +46,8 @@ export class ListCorePlugin extends OdooCorePlugin {
         this.nextId = 1;
         /** @type {Object.<string, ListDefinition>} */
         this.lists = {};
+        /** @type {Object.<string, Object.<string, ComputeState>>} */
+        this.compiledColumnFormulas = {};
     }
 
     /**
@@ -130,6 +141,7 @@ export class ListCorePlugin extends OdooCorePlugin {
             }
             case "UPDATE_ODOO_LIST": {
                 this.history.update("lists", cmd.listId, cmd.list);
+                this._compileCalculatedColumns(cmd.listId, cmd.list);
                 break;
             }
         }
@@ -192,13 +204,76 @@ export class ListCorePlugin extends OdooCorePlugin {
         return id in this.lists;
     }
 
+    getListCompiledColumnFormula(listId, columnName) {
+        return this.compiledColumnFormulas[listId]?.[columnName]?.formula;
+    }
+
+    getListCompiledColumnDependencies(listId, columnName) {
+        return this.compiledColumnFormulas[listId]?.[columnName]?.dependencies;
+    }
+
     // ---------------------------------------------------------------------
     // Private
     // ---------------------------------------------------------------------
 
     _addList(id, definition) {
         this.history.update("lists", id, definition);
+        this._compileCalculatedColumns(id, definition);
         this.history.update("nextId", parseInt(id, 10) + 1);
+    }
+
+    _compileCalculatedColumns(listId, definition) {
+        const computedColumns = definition.columns.filter((col) => col.computedBy);
+        for (const column of computedColumns) {
+            const compiledFormula = CompiledFormula.Compile(
+                column.computedBy.formula,
+                column.computedBy.sheetId,
+                this.getters
+            );
+            this.history.update(
+                "compiledColumnFormulas",
+                listId,
+                column.name,
+                "formula",
+                compiledFormula
+            );
+        }
+        for (const column of computedColumns) {
+            const dependencies = this._computeColumnFullDependencies(listId, definition, column);
+            this.history.update(
+                "compiledColumnFormulas",
+                listId,
+                column.name,
+                "dependencies",
+                dependencies
+            );
+        }
+    }
+
+    _computeColumnFullDependencies(listId, definition, column, exploredCols = new Set()) {
+        const rangeDependencies = [];
+        const formula = this.compiledColumnFormulas[listId]?.[column.name]?.formula;
+        exploredCols.add(column.name);
+        for (const usedSymbols of formula.symbols) {
+            const otherCol = definition.columns.find(
+                (columnCandidate) =>
+                    getCanonicalSymbolName(columnCandidate.name) === usedSymbols &&
+                    column.name !== columnCandidate.name
+            );
+
+            if (!otherCol || !otherCol.computedBy || exploredCols.has(otherCol.name)) {
+                continue;
+            }
+
+            rangeDependencies.push(
+                ...this._computeColumnFullDependencies(listId, definition, otherCol, exploredCols)
+            );
+        }
+        rangeDependencies.push(...formula.rangeDependencies.filter((range) => !range.invalidXc));
+        rangeDependencies.push(
+            ...formula.getNamedRangesInFormula(this.getters).map((namedRange) => namedRange.range)
+        );
+        return rangeDependencies;
     }
 
     /**