dc650a61302dcc8d77798d24b145d66c9ab660d5
Adrien Minne (adrm) <adrm@odoo.com>
2026-05-05 13:53:51 +0200

    [IMP] spreadsheet: add side panel for unused pivots/lists
    This commit replace the `Unused pivot/list` warning in the `Data` topbar
    menu by a dedicated side panel. Checking which pivot is unused is an
    expansive operation, and is not frankly important, removing the pivots
    only cleans up the JSON. Doing it every time we open the menu can
    make the menu very slow on big spreadsheets.
    
    There was also a problem on how we were checking which pivot was unused.
    Only the formulas in the cells were checked, not the formulas of
    cfs/charts/... This commit introduce a new `FormulaProvider` to the
    core plugins, so they can register their formulas.
    
    Task: 6185119

--- addons/spreadsheet/static/src/list/plugins/list_core_view_plugin.js
+++ addons/spreadsheet/static/src/list/plugins/list_core_view_plugin.js
@@ -1,11 +1,11 @@
 import * as spreadsheet from "@odoo/o-spreadsheet";
-import { getFirstListFunction } from "../list_helpers";
+import { getFirstListFunction, getListFunctions } from "../list_helpers";
 import { Domain } from "@web/core/domain";
 import { ListDataSource } from "../list_data_source";
 import { OdooCoreViewPlugin } from "@spreadsheet/plugins";
 import { isDataSourceUrl, parseDataSourceUrl } from "../../data_sources/data_source_link";
 
-const { astToFormula } = spreadsheet;
+const { astToFormula, isCoreCommand } = spreadsheet;
 const { isEvaluationError } = spreadsheet.helpers;
 const { isMarkdownLink, parseMarkdownLink } = spreadsheet.links;
 
@@ -57,6 +57,10 @@ export class ListCoreViewPlugin extends OdooCoreViewPlugin {
      * @param {Object} cmd Command
      */
     handle(cmd) {
+        if (isCoreCommand(cmd) || cmd.type === "UNDO" || cmd.type === "REDO") {
+            this.unusedLists = undefined;
+        }
+
         switch (cmd.type) {
             case "INSERT_ODOO_LIST": {
                 const { listId, linesNumber } = cmd;
@@ -195,17 +199,19 @@ export class ListCoreViewPlugin extends OdooCoreViewPlugin {
             return this.unusedLists;
         }
         const unusedLists = new Set(this.getters.getListIds());
+
+        for (const formula of this.getters.getAllFormulas()) {
+            for (const listId of this.getListIdsFromFormula(formula)) {
+                unusedLists.delete(listId);
+                if (!unusedLists.size) {
+                    this.unusedLists = [];
+                    return this.unusedLists;
+                }
+            }
+        }
+
         for (const sheetId of this.getters.getSheetIds()) {
             for (const cell of this.getters.getCells(sheetId)) {
-                const position = this.getters.getCellPosition(cell.id);
-                const listId = this.getListIdFromPosition(position);
-                if (listId) {
-                    unusedLists.delete(listId);
-                    if (!unusedLists.size) {
-                        this.unusedLists = [];
-                        return this.unusedLists;
-                    }
-                }
                 if (isMarkdownLink(cell.content)) {
                     const { url } = parseMarkdownLink(cell.content);
                     if (isDataSourceUrl(url)) {
@@ -291,15 +297,18 @@ export class ListCoreViewPlugin extends OdooCoreViewPlugin {
      */
     getListIdFromPosition(position) {
         const cell = this.getters.getCorrespondingFormulaCell(position);
-        const sheetId = position.sheetId;
         if (cell && cell.isFormula) {
-            const listFunction = getFirstListFunction(cell.compiledFormula, this.getters);
-            if (listFunction) {
-                const content = astToFormula(listFunction.args[0]);
-                return this.getters.evaluateFormula(sheetId, content)?.toString();
-            }
+            return this.getListIdsFromFormula(cell.compiledFormula)[0];
         }
         return undefined;
+    }
+
+    getListIdsFromFormula(compiledFormula) {
+        const listFunctions = getListFunctions(compiledFormula, this.getters);
+        return listFunctions.map((listFunction) => {
+            const content = astToFormula(listFunction.args[0]);
+            return this.getters.evaluateFormula(compiledFormula.sheetId, content)?.toString();
+        });
     }
 
     getListFieldFromPosition(position) {
