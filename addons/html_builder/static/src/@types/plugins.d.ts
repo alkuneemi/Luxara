dzdzdzdz1010/OdooStandard

wip [FIX] make save_handlers robust to undo after save
note: accidentaly, I made normalize not run on the full wrap at first edit (I did not realize until it failed some tests because of that)

note: I noticed that panel design mess was not saved if we were not "focused" on the thing were it can appear when we save

addons/html_builder/static/src/@types/plugins.d.ts
@@ -7,13 +7,14 @@ declare module "plugins" {
     import { CloneShared, on_cloned_handlers, on_will_clone_handlers } from "@html_builder/core/clone_plugin";
     import { CustomizeTabShared } from "@html_builder/core/customize_tab_plugin";
     import { DisableSnippetsShared } from "@html_builder/core/disable_snippets_plugin";
+    import { dirt_marks } from "@html_builder/core/dirt_mark_plugin";
     import { dropzone_selector, DropZoneShared, filter_for_sibling_dropzone_predicates } from "@html_builder/core/drop_zone_plugin";
     import { after_replication_handlers } from "@html_builder/core/field_change_replication_plugin";
     import { MediaWebsiteShared } from "@html_builder/core/media_website_plugin";
     import { OperationShared } from "@html_builder/core/operation_plugin";
     import { get_overlay_buttons, OverlayButtonsShared } from "@html_builder/core/overlay_buttons/overlay_buttons_plugin";
     import { empty_node_predicates, is_unremovable_selector, on_removed_handlers, on_will_remove_handlers, RemoveShared } from "@html_builder/core/remove_plugin";
-    import { after_save_handlers, before_save_handlers, get_dirty_els, save_element_handlers, save_elements_overrides, save_handlers, SaveShared } from "@html_builder/core/save_plugin";
+    import { after_save_handlers, before_save_handlers, save_handlers, SaveShared } from "@html_builder/core/save_plugin";
     import { after_setup_editor_handlers, before_setup_editor_handlers, savable_selectors, SetupEditorShared } from "@html_builder/core/setup_editor_plugin";
     import { target_hide, target_show, VisibilityShared } from "@html_builder/core/visibility_plugin";
     import { default_shape_handlers, image_shape_groups_providers, post_compute_shape_listeners } from "@html_builder/plugins/image/image_shape_option_plugin";
@@ -44,6 +45,7 @@ declare module "plugins" {
         cachedModel: CachedModelShared;
         clone: CloneShared;
         customizeTab: CustomizeTabShared;
+        dirtMark: DirtMarkShared,
         disableSnippets: DisableSnippetsShared;
         dropzone: DropZoneShared;
         media_website: MediaWebsiteShared;
@@ -95,7 +97,6 @@ declare module "plugins" {
         on_will_clone_handlers: on_will_clone_handlers;
         on_will_remove_handlers: on_will_remove_handlers;
         post_compute_shape_listeners: post_compute_shape_listeners;
-        save_element_handlers: save_element_handlers;
         save_handlers: save_handlers;
         snippet_preview_dialog_stylesheets_handlers: snippet_preview_dialog_stylesheets_handlers;
         target_hide: target_hide;
@@ -104,7 +105,6 @@ declare module "plugins" {
 
         // Overrides
         apply_custom_css_style: apply_custom_css_style;
-        save_elements_overrides: save_elements_overrides;
 
         // Predicates
         empty_node_predicates: empty_node_predicates;
@@ -118,7 +118,6 @@ declare module "plugins" {
         background_filter_target_providers: background_filter_target_providers;
         background_shape_target_providers: background_shape_target_providers;
         clone_disabled_reason_providers: clone_disabled_reason_providers;
-        get_dirty_els: get_dirty_els;
         get_options_container_top_buttons: get_options_container_top_buttons;
         get_target_element_providers: get_target_element_providers;
         remove_disabled_reason_providers: remove_disabled_reason_providers;
@@ -131,6 +130,7 @@ declare module "plugins" {
         container_title: container_title;
         content_editable_selectors: content_editable_selectors;
         content_not_editable_selectors: content_not_editable_selectors;
+        dirt_marks: dirt_marks;
         dropzone_selector: dropzone_selector;
         elements_to_options_title_components: elements_to_options_title_components;
         fontCssVariables: fontCssVariables;

alkuneemi/Luxara

addons/html_builder/static/src/@types/plugins.d.ts
@@ -6,14 +6,16 @@ declare module "plugins" {
     import { CachedModelShared } from "@html_builder/core/cached_model_plugin";
     import { CloneShared, on_cloned_handlers, on_will_clone_handlers } from "@html_builder/core/clone_plugin";
     import { CustomizeTabShared } from "@html_builder/core/customize_tab_plugin";
+    import { DirtMarkShared } from "@html_builder/core/dirt_mark_plugin";
     import { DisableSnippetsShared } from "@html_builder/core/disable_snippets_plugin";
+    import { dirt_marks } from "@html_builder/core/dirt_mark_plugin";
     import { dropzone_selectors, DropZoneShared, is_valid_for_sibling_dropzone_predicates } from "@html_builder/core/drop_zone_plugin";
     import { on_replicated_handlers } from "@html_builder/core/field_change_replication_plugin";
     import { MediaWebsiteShared } from "@html_builder/core/media_website_plugin";
     import { OperationShared } from "@html_builder/core/operation_plugin";
     import { get_overlay_buttons, OverlayButtonsShared, should_show_overlay_buttons_of_ancestor_predicates } from "@html_builder/core/overlay_buttons/overlay_buttons_plugin";
     import { is_node_empty_predicates, is_unremovable_selectors, on_removed_handlers, on_will_remove_handlers, RemoveShared } from "@html_builder/core/remove_plugin";
-    import { on_saved_handlers, on_will_save_handlers, dirty_els_providers, on_will_save_element_handlers, save_elements_overrides, on_ready_to_save_document_handlers, SaveShared } from "@html_builder/core/save_plugin";
+    import { on_saved_handlers, on_will_save_handlers, on_ready_to_save_document_handlers, SaveShared } from "@html_builder/core/save_plugin";
     import { after_setup_editor_overrides, on_will_setup_editor_handlers, savable_selectors, SetupEditorShared } from "@html_builder/core/setup_editor_plugin";
     import { on_target_hidden_handlers, on_target_shown_handlers, VisibilityShared } from "@html_builder/core/visibility_plugin";
     import { default_shape_providers, image_shape_groups_providers, on_shape_computed_handlers } from "@html_builder/plugins/image/image_shape_option_plugin";
@@ -47,6 +49,7 @@ declare module "plugins" {
         cachedModel: CachedModelShared;
         clone: CloneShared;
         customizeTab: CustomizeTabShared;
+        dirtMark: DirtMarkShared,
         disableSnippets: DisableSnippetsShared;
         dropzone: DropZoneShared;
         media_website: MediaWebsiteShared;
@@ -98,14 +101,12 @@ declare module "plugins" {
         on_will_remove_handlers: on_will_remove_handlers;
         on_will_restore_containers_handlers: on_will_restore_containers_handlers;
         on_will_save_handlers: on_will_save_handlers;
-        on_will_save_element_handlers: on_will_save_element_handlers;
         on_will_setup_editor_handlers: on_will_setup_editor_handlers;
         on_ready_to_save_document_handlers: on_ready_to_save_document_handlers;
 
         // Overrides
         after_setup_editor_overrides: after_setup_editor_overrides;
         apply_custom_css_style_overrides: apply_custom_css_style_overrides;
-        save_elements_overrides: save_elements_overrides;
 
         // Predicates
         should_keep_overlay_options_predicates: should_keep_overlay_options_predicates;
@@ -127,7 +128,6 @@ declare module "plugins" {
         background_shape_target_providers: background_shape_target_providers;
         clone_disabled_reason_providers: clone_disabled_reason_providers;
         default_shape_providers: default_shape_providers;
-        dirty_els_providers: dirty_els_providers;
         image_shape_groups_providers: image_shape_groups_providers;
         options_container_top_buttons_providers: options_container_top_buttons_providers;
         remove_disabled_reason_providers: remove_disabled_reason_providers;
@@ -141,6 +141,7 @@ declare module "plugins" {
         content_editable_selectors: content_editable_selectors;
         content_not_editable_selectors: content_not_editable_selectors;
         dropzone_selectors: dropzone_selectors;
+        dirt_marks: dirt_marks;
         elements_to_options_title_components: elements_to_options_title_components;
         fontCssVariables: fontCssVariables;
         get_overlay_buttons: get_overlay_buttons;