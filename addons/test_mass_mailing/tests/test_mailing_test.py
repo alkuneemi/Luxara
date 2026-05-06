odoo/odoo

[FIX] mass_mailing: unlink mail.message of test mails
**Steps to reproduce:**
- Go to Email Marketing app
- Create a mailing campaign
- Set its recipients to Contact
- Click on the test button to send a test mail to any mail
- Go to the first contact record
- Chatter will show `This message has been removed` message

**Issue:**
Previously, message created for testing were ignored
by the Chatter as they were empty. As we now keep empty messages
visible but with removed content display, they shows up on
related records.

**Fix:**
Ensure the related messages are unlinked at the same time as
the test mail in `send_mail_test` by setting `is_notification`
to False to trigger the `unlink` logic.

related: https://github.com/odoo/odoo/commit/21f92550f83cbd38df2c223c65c61bd16dc8e2b0

opw-5502787

addons/test_mass_mailing/tests/test_mailing_test.py
@@ -160,6 +160,21 @@ class TestMailingTest(TestMassMailCommon):
             "Should use the value of the previous record's email_to as default",
         )
 
+        # Also test that related messages were properly deleted
+        mailing.subject = 'Dummy Subject'
+
+        with self.mock_mail_gateway(mail_unlink_sent=True):
+            mailing_test.send_mail_test()
+
+        test_subject = '[TEST] %s' % mailing.subject
+        self.assertSentEmail(
+            self.env.user.partner_id,
+            ['test@test.com'],
+            subject=test_subject,
+        )
+        self.assertFalse(self.env['mail.mail'].search([('subject', '=', test_subject)]))
+        self.assertFalse(self.env['mail.message'].search([('subject', '=', test_subject)]))
+
 
 @tagged('mailing_manage', 'twilio')
 class TestMailingSMSTest(TestMassSMSCommon, MockSmsTwilioApi):

alkuneemi/Luxara

- Upload a file in Settings > Attach a file
- Related attachment appears in the chatter
Before 18.2, messages created for testing were ignored
by the Chatter as they were empty (and not unlinked).
But if an attachment was provided, it was linked to the
test message and not deleted afterwards (which means
it shows up in the record chatter).
to False to trigger the `unlink` logic and remove the related
attachments at the same time.
backport of: https://github.com/odoo/odoo/commit/526b3d73886558315f2435714b2ed82fec313e78
opw-6168632
X-original-commit: ac032574b7480a8a9ef66a9f8d6e541da7f4c5b5

addons/test_mass_mailing/tests/test_mailing_test.py
@@ -129,13 +129,16 @@ class TestMailingTest(TestMassMailCommon):
             'mass_mailing_id': mailing.id,
         })
 
-        with self.mock_mail_gateway():
+        with self.mock_mail_gateway(mail_unlink_sent=True):
             mailing_test.send_mail_test()
 
         expected_test_record = self.env[mailing.mailing_model_real].search([], limit=1)
         self.assertEqual(expected_test_record, self.test_records[0], 'Should take first found one')
         expected_subject = f'Subject {expected_test_record.name} <t t-out="object.name"/>'
         expected_body = 'Hello {{ object.name }}' + f' {expected_test_record.name}'
+        # Also test that related messages were properly deleted
+        self.assertFalse(self.env['mail.mail'].search([('subject', '=', expected_subject)]))
+        self.assertFalse(self.env['mail.message'].search([('subject', '=', expected_subject)]))
 
         self.assertSentEmail(self.env.user.partner_id, ['test@test.com'],
             subject='[TEST] %s' % expected_subject,