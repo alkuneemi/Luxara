MoLeap/odooo

[IMP] mail, *: improve mail/data requests API
*: hr, im_livechat, portal, website_livechat

Before this commit, we would need to register mail/data entries in an if
statement based on the name and whether it was applied to logged in
users or internal.

The point of this PR is to improve the API to be able to have clear
parameters and easy registry of the different mail/data request type.

This is done with the help of a registry and a decorator to be added on
the different request types.

task-6085653

addons/hr/controllers/webclient.py
@@ -2,16 +2,15 @@
 
 from odoo.addons.mail.controllers.webclient import WebclientController
 from odoo.addons.mail.tools.discuss import Store
+from odoo.addons.mail.tools.mail_data_registry import mail_data_handler
 from odoo.http import request
 
 
 class HrWebclientController(WebclientController):
-    @classmethod
-    def _process_request_for_internal_user(cls, store: Store, name, params):
-        super()._process_request_for_internal_user(store, name, params)
-        if name == "hr.employee.public":
-            emp_public = request.env["hr.employee.public"].search_fetch([("id", "=", params["id"])])
-            store.add(emp_public, "_store_avatar_card_fields")
+    @mail_data_handler("hr.employee.public")
+    def _mail_data_hr_employee_public(self, store: Store, id):
+        emp_public = request.env["hr.employee.public"].search_fetch([("id", "=", id)])
+        store.add(emp_public, "_store_avatar_card_fields")
 
     @classmethod
     def _get_supported_avatar_card_models(self):

addons/mail/controllers/mailbox.py
@@ -4,42 +4,68 @@ from odoo.fields import Command, Domain
 from odoo.http import request
 from odoo.addons.mail.controllers.webclient import WebclientController
 from odoo.addons.mail.tools.discuss import Store
+from odoo.addons.mail.tools.mail_data_registry import mail_data_handler
 
 
 class MailboxController(WebclientController):
     @classmethod
-    def _process_request_for_logged_in_user(self, store: Store, name, params):
-        """Override to mailbox messages."""
-        super()._process_request_for_logged_in_user(store, name, params)
+    def _update_bookmark(cls, store, message_id, bookmarked):
         bookmark_messages = request.env["mail.message"]
-        if name in ["add_bookmark", "remove_bookmark"]:
-            if message := self._get_message_with_access(params["message_id"], mode="read"):
-                command = Command.link if name == "add_bookmark" else Command.unlink
-                # sudo: mail.message - users can bookmark messages they can read
-                message.sudo().bookmarked_partner_ids = [command(request.env.user.partner_id.id)]
-                bookmark_messages |= message
-        if name == "remove_all_bookmarks":
-            # sudo: mail.message - users can remove their bookmarks
-            messages_su = request.env["mail.message"].sudo().search_fetch([("is_bookmarked", "=", True)])
-            messages_su.bookmarked_partner_ids = [Command.unlink(request.env.user.partner_id.id)]
-            bookmark_messages |= messages_su
-        if bookmark_messages:
-            bus_store = Store(bus_channel=request.env.user)
-            for cur_store in [store, bus_store]:
-                cur_store.add(bookmark_messages, ["is_bookmarked"])
-                cur_store.add_global_values(request.env.user._store_bookmark_box_global_fields)
-            bus_store.bus_send()
-        message_fetch_domain = None
-        if name == "/mail/inbox/messages":
-            message_fetch_domain = Domain("needaction", "=", True)
-            request.update_context(add_inbox_fields=True)
-        if name == "/mail/history/messages":
-            message_fetch_domain = Domain("needaction", "=", False)
-        if name == "/mail/bookmark/messages":
-            message_fetch_domain = Domain("bookmarked_partner_ids", "in", [request.env.user.partner_id.id])
-        if message_fetch_domain:
-            self._resolve_messages(
-                store,
-                domain=message_fetch_domain,
-                fetch_params=params and params.get("fetch_params"),
-            )
+        if message := cls._get_message_with_access(message_id, mode="read"):
+            command = Command.link if bookmarked else Command.unlink
+            # sudo: mail.message - users can bookmark messages they can read
+            message.sudo().bookmarked_partner_ids = [command(request.env.user.partner_id.id)]
+            bookmark_messages |= message
+        cls._send_bookmark_update(store, bookmark_messages)
+
+    @classmethod
+    def _send_bookmark_update(cls, store, messages):
+        if not messages:
+            return
+        bus_store = Store(bus_channel=request.env.user)
+        for cur_store in [store, bus_store]:
+            cur_store.add(messages, ["is_bookmarked"])
+            cur_store.add_global_values(request.env.user._store_bookmark_box_global_fields)
+        bus_store.bus_send()
+
+    @mail_data_handler("add_bookmark")
+    def _mail_data_add_bookmark(self, store: Store, message_id):
+        self._update_bookmark(store, message_id, bookmarked=True)
+
+    @mail_data_handler("remove_bookmark")
+    def _mail_data_remove_bookmark(self, store: Store, message_id):
+        self._update_bookmark(store, message_id, bookmarked=False)
+
+    @mail_data_handler("remove_all_bookmarks")
+    def _mail_data_remove_all_bookmarks(self, store: Store):
+        # sudo: mail.message - users can remove their bookmarks
+        messages_su = (
+            request.env["mail.message"].sudo().search_fetch([("is_bookmarked", "=", True)])
+        )
+        messages_su.bookmarked_partner_ids = [Command.unlink(request.env.user.partner_id.id)]
+        self._send_bookmark_update(store, messages_su)
+
+    @mail_data_handler("/mail/inbox/messages")
+    def _mail_data_mailbox_messages(self, store: Store, fetch_params=None):
+        request.update_context(add_inbox_fields=True)
+        self._resolve_messages(
+            store,
+            domain=Domain("needaction", "=", True),
+            fetch_params=fetch_params,
+        )
+
+    @mail_data_handler("/mail/history/messages")
+    def _mail_data_history_messages(self, store: Store, fetch_params=None):
+        self._resolve_messages(
+            store,
+            domain=Domain("needaction", "=", False),
+            fetch_params=fetch_params,
+        )
+
+    @mail_data_handler("/mail/bookmark/messages")
+    def _mail_data_bookmark_messages(self, store: Store, fetch_params=None):
+        self._resolve_messages(
+            store,
+            domain=Domain("bookmarked_partner_ids", "in", [request.env.user.partner_id.id]),
+            fetch_params=fetch_params,
+        )

alkuneemi/Luxara

addons/hr/controllers/webclient.py
@@ -2,16 +2,15 @@
 
 from odoo.addons.mail.controllers.webclient import WebclientController
 from odoo.addons.mail.tools.discuss import Store
+from odoo.addons.mail.tools.store_handler import store_handler
 from odoo.http import request
 
 
 class HrWebclientController(WebclientController):
-    @classmethod
-    def _process_request_for_internal_user(cls, store: Store, name, params):
-        super()._process_request_for_internal_user(store, name, params)
-        if name == "hr.employee.public":
-            emp_public = request.env["hr.employee.public"].search_fetch([("id", "=", params["id"])])
-            store.add(emp_public, "_store_avatar_card_fields")
+    @store_handler("hr.employee.public")
+    def store_hr_employee_public(self, store: Store, id):
+        emp_public = request.env["hr.employee.public"].search_fetch([("id", "=", id)])
+        store.add(emp_public, "_store_avatar_card_fields")
 
     @classmethod
     def _get_supported_avatar_card_models(self):

addons/mail/controllers/mailbox.py
@@ -2,45 +2,69 @@
 
 from odoo.fields import Command, Domain
 from odoo.http import request
-from odoo.addons.mail.controllers.webclient import WebclientController, WRITE_FETCH_PARAMS
+from odoo.addons.mail.controllers.webclient import WebclientController
 from odoo.addons.mail.tools.discuss import Store
-
-WRITE_FETCH_PARAMS |= {"add_bookmark", "remove_bookmark", "remove_all_bookmarks"}
+from odoo.addons.mail.tools.store_handler import store_handler
 
 
 class MailboxController(WebclientController):
     @classmethod
-    def _process_request_for_logged_in_user(self, store: Store, name, params):
-        """Override to mailbox messages."""
-        super()._process_request_for_logged_in_user(store, name, params)
+    def _update_bookmark(cls, store, message_id, bookmarked):
         bookmark_messages = request.env["mail.message"]
-        if name in ["add_bookmark", "remove_bookmark"]:
-            if message := self._get_message_with_access(params["message_id"], mode="read"):
-                command = Command.link if name == "add_bookmark" else Command.unlink
-                # sudo: mail.message - users can bookmark messages they can read
-                message.sudo().bookmarked_partner_ids = [command(request.env.user.partner_id.id)]
-                bookmark_messages |= message
-        if name == "remove_all_bookmarks":
-            # sudo: mail.message - users can remove their bookmarks
-            messages_su = request.env["mail.message"].sudo().search_fetch([("is_bookmarked", "=", True)])
-            messages_su.bookmarked_partner_ids = [Command.unlink(request.env.user.partner_id.id)]
-            bookmark_messages |= messages_su
-        if bookmark_messages:
-            bus_store = Store(bus_channel=request.env.user)
-            for cur_store in [store, bus_store]:
-                cur_store.add(bookmark_messages, ["is_bookmarked"])
-                cur_store.add_global_values(request.env.user._store_bookmark_box_global_fields)
-        message_fetch_domain = None
-        if name == "/mail/inbox/messages":
-            message_fetch_domain = Domain("needaction", "=", True)
-            request.update_context(add_inbox_fields=True)
-        if name == "/mail/history/messages":
-            message_fetch_domain = Domain("needaction", "=", False)
-        if name == "/mail/bookmark/messages":
-            message_fetch_domain = Domain("bookmarked_partner_ids", "in", [request.env.user.partner_id.id])
-        if message_fetch_domain:
-            self._resolve_messages(
-                store,
-                domain=message_fetch_domain,
-                fetch_params=params and params.get("fetch_params"),
-            )
+        if message := cls._get_message_with_access(message_id, mode="read"):
+            command = Command.link if bookmarked else Command.unlink
+            # sudo: mail.message - users can bookmark messages they can read
+            message.sudo().bookmarked_partner_ids = [command(request.env.user.partner_id.id)]
+            bookmark_messages |= message
+        cls._send_bookmark_update(store, bookmark_messages)
+
+    @classmethod
+    def _send_bookmark_update(cls, store, messages):
+        if not messages:
+            return
+        bus_store = Store(bus_channel=request.env.user)
+        for cur_store in [store, bus_store]:
+            cur_store.add(messages, ["is_bookmarked"])
+            cur_store.add_global_values(request.env.user._store_bookmark_box_global_fields)
+
+    @store_handler("add_bookmark", readonly=False)
+    def store_add_bookmark(self, store: Store, message_id):
+        self._update_bookmark(store, message_id, bookmarked=True)
+
+    @store_handler("remove_bookmark", readonly=False)
+    def store_remove_bookmark(self, store: Store, message_id):
+        self._update_bookmark(store, message_id, bookmarked=False)
+
+    @store_handler("remove_all_bookmarks", readonly=False)
+    def store_remove_all_bookmarks(self, store: Store):
+        # sudo: mail.message - users can remove their bookmarks
+        messages_su = (
+            request.env["mail.message"].sudo().search_fetch([("is_bookmarked", "=", True)])
+        )
+        messages_su.bookmarked_partner_ids = [Command.unlink(request.env.user.partner_id.id)]
+        self._send_bookmark_update(store, messages_su)
+
+    @store_handler("/mail/inbox/messages")
+    def store_mailbox_messages(self, store: Store, fetch_params=None):
+        request.update_context(add_inbox_fields=True)
+        self._resolve_messages(
+            store,
+            domain=Domain("needaction", "=", True),
+            fetch_params=fetch_params,
+        )
+
+    @store_handler("/mail/history/messages")
+    def store_history_messages(self, store: Store, fetch_params=None):
+        self._resolve_messages(
+            store,
+            domain=Domain("needaction", "=", False),
+            fetch_params=fetch_params,
+        )
+
+    @store_handler("/mail/bookmark/messages")
+    def store_bookmark_messages(self, store: Store, fetch_params=None):
+        self._resolve_messages(
+            store,
+            domain=Domain("bookmarked_partner_ids", "in", [request.env.user.partner_id.id]),
+            fetch_params=fetch_params,
+        )