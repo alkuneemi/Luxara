MoLeap/odooo

[IMP] digest, *: improve digest KPI actions to open relevant views
Update several Digest KPI actions across modules to open views that better
match the metrics displayed in the Digest email. The actions now redirect to
more relevant analytical views (e.g., graph/pivot) so that when users click
"Open Report", they see data consistent with the computed Digest KPI.

task-5409170

addons/account/models/digest.py
@@ -33,6 +33,8 @@ class DigestDigest(models.Model):
     def _get_kpi_custom_settings(self, company, user):
         res = super()._get_kpi_custom_settings(company, user)
         menu_id = self.env.ref('account.menu_finance').id
-        res['kpi_action']['kpi_account_total_revenue'] = f'account.action_move_out_invoice_type?menu_id={menu_id}'
+        res['kpi_action']['kpi_account_total_revenue'] = (
+            f'account.action_account_invoice_report_all?menu_id={menu_id}'
+        )
         res['kpi_sequence']['kpi_account_total_revenue'] = 5500
         return res

addons/im_livechat/models/digest.py
@@ -11,7 +11,7 @@ class DigestDigest(models.Model):
     kpi_livechat_rating_value = fields.Float(digits=(16, 2), compute='_compute_kpi_livechat_rating_value')
     kpi_livechat_conversations = fields.Boolean('Conversations handled')
     kpi_livechat_conversations_value = fields.Integer(compute='_compute_kpi_livechat_conversations_value')
-    kpi_livechat_response = fields.Boolean('Time to answer (sec)')
+    kpi_livechat_response = fields.Boolean('Response Time')
     kpi_livechat_response_value = fields.Float(digits=(16, 2), compute='_compute_kpi_livechat_response_value')
 
     def _compute_kpi_livechat_rating_value(self):
@@ -47,6 +47,7 @@ class DigestDigest(models.Model):
 
     def _get_kpi_custom_settings(self, company, user):
         res = super()._get_kpi_custom_settings(company, user)
+        res['kpi_action']['kpi_livechat_rating'] = 'im_livechat.im_livechat_report_channel_action'
         res['kpi_action']['kpi_livechat_conversations'] = 'im_livechat.im_livechat_report_channel_action'
         res['kpi_action']['kpi_livechat_response'] = 'im_livechat.im_livechat_report_channel_time_to_answer_action'
         res['is_cross_company'].update(('kpi_livechat_rating', 'kpi_livechat_conversations', 'kpi_livechat_response'))

alkuneemi/Luxara

addons/account/models/digest.py
@@ -33,6 +33,8 @@ class DigestDigest(models.Model):
     def _get_kpi_custom_settings(self, company, user):
         res = super()._get_kpi_custom_settings(company, user)
         menu_id = self.env.ref('account.menu_finance').id
-        res['kpi_action']['kpi_account_total_revenue'] = f'account.action_move_out_invoice_type?menu_id={menu_id}'
+        res['kpi_action']['kpi_account_total_revenue'] = (
+            f'account.action_account_invoice_report_all_digest?menu_id={menu_id}'
+        )
         res['kpi_sequence']['kpi_account_total_revenue'] = 5500
         return res

addons/im_livechat/models/digest.py
@@ -11,7 +11,7 @@ class DigestDigest(models.Model):
     kpi_livechat_rating_value = fields.Float(digits=(16, 2), compute='_compute_kpi_livechat_rating_value')
     kpi_livechat_conversations = fields.Boolean('Conversations handled')
     kpi_livechat_conversations_value = fields.Integer(compute='_compute_kpi_livechat_conversations_value')
-    kpi_livechat_response = fields.Boolean('Time to answer (sec)')
+    kpi_livechat_response = fields.Boolean('Response Time')
     kpi_livechat_response_value = fields.Float(digits=(16, 2), compute='_compute_kpi_livechat_response_value')
 
     def _compute_kpi_livechat_rating_value(self):
@@ -59,6 +59,7 @@ class DigestDigest(models.Model):
 
     def _get_kpi_custom_settings(self, company, user):
         res = super()._get_kpi_custom_settings(company, user)
+        res['kpi_action']['kpi_livechat_rating'] = 'im_livechat.im_livechat_report_channel_rating_action'
         res['kpi_action']['kpi_livechat_conversations'] = 'im_livechat.im_livechat_report_channel_action'
         res['kpi_action']['kpi_livechat_response'] = 'im_livechat.im_livechat_report_channel_time_to_answer_action'
         res['is_cross_company'].update(('kpi_livechat_rating', 'kpi_livechat_conversations', 'kpi_livechat_response'))