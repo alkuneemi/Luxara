3288b16fc73db10049190229289fc1a426aef1c6
Ruben Gomes (rugo) <rugo@odoo.com>
2026-05-07 13:11:12 +0200

    [REF] test_orm: remove base models dependencies from test_models

--- odoo/addons/test_orm/__manifest__.py
+++ odoo/addons/test_orm/__manifest__.py
@@ -8,6 +8,7 @@
         'data/test_action_bindings.xml',
         'data/test_inherits.xml',
         'data/test_orm_data.xml',
+        'data/test_orm_partner.xml',
         'data/test_translated_field/test_model_data.xml',
         'security/ir.model.access.csv',
         'security/test_access_rights_security.xml',
