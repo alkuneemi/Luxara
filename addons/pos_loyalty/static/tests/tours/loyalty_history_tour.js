odoo/odoo

[FIX] pos_loyalty: record gross issued/used points in loyalty history
When a loyalty card both earned and spent points in the same POS order,
the history entry only reflected the net difference instead of the gross
amounts.

The root cause was that the JS payload sent only a single `points` field
representing the net change. Fix by tracking `points_earned` and
`points_spent` separately in `couponData` and sending them to the
server.

opw-6041420

addons/pos_loyalty/static/tests/tours/loyalty_history_tour.js
@@ -17,3 +17,21 @@ registry.category("web_tour.tours").add("LoyaltyHistoryTour", {
             PosLoyalty.finalizeOrder("Cash", "10"),
         ].flat(),
 });
+
+registry.category("web_tour.tours").add("test_loyalty_history_earn_and_spend", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+            // Select the partner first so their pre-loaded points are visible
+            ProductScreen.clickPartnerButton(),
+            ProductScreen.clickCustomer("AAA Test Partner"),
+            // Buy a $10 product — this earns 10 points
+            ProductScreen.addOrderline("Whiteboard Pen", "1"),
+            // Claim the reward that costs 5 points (10% discount on the order)
+            PosLoyalty.claimReward("10% on your order"),
+            // $10 - 10% = $9.00
+            PosLoyalty.orderTotalIs("9.00"),
+            PosLoyalty.finalizeOrder("Cash", "9"),
+        ].flat(),
+});

alkuneemi/Luxara

X-original-commit: 4f35ecce51becb1bc1442c6a39bb1b4d31093291

addons/pos_loyalty/static/tests/tours/loyalty_history_tour.js
@@ -17,3 +17,21 @@ registry.category("web_tour.tours").add("LoyaltyHistoryTour", {
             PosLoyalty.finalizeOrder("Cash", "10"),
         ].flat(),
 });
+
+registry.category("web_tour.tours").add("test_loyalty_history_earn_and_spend", {
+    steps: () =>
+        [
+            Chrome.startPoS(),
+            Dialog.confirm("Open Register"),
+            // Select the partner first so their pre-loaded points are visible
+            ProductScreen.clickPartnerButton(),
+            ProductScreen.clickCustomer("AAA Test Partner"),
+            // Buy a $10 product — this earns 10 points
+            ProductScreen.addOrderline("Test Product", "1"),
+            // Claim the reward that costs 5 points (10% discount on the order)
+            PosLoyalty.claimReward("10% on your order"),
+            // $10 - 10% = $9.00
+            PosLoyalty.orderTotalIs("9.00"),
+            PosLoyalty.finalizeOrder("Cash", "9"),
+        ].flat(),
+});