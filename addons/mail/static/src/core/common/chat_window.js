odoo/odoo

[FIX] mail,im_livechat: mark ended livechat conversations as read
Purpose of this Commit:
Previously, when a livechat conversation ended, it was never automatically
marked as read. The existing `mark_as_read` mechanism depends on the
composer being focused, but ended livechat conversations hides the composer,
and the chat window does not focus the thread automatically
(focus only happens on explicit click).

This made it impossible for the read state to be triggered through the normal
path, leaving agents with persistent unread indicators on closed livechat
conversations.

This commit updates the autofocus behavior to:
- Focus the composer when present.
- Focus the conversation otherwise.

This ensures the read state is correctly triggered when the conversation
is effectively in focus.

task-5900038

addons/mail/static/src/core/common/chat_window.js
@@ -64,6 +64,21 @@ export class ChatWindow extends Component {
         });
     }
 
+    get autofocusComposer() {
+        if (this.isMobileOS || this.thread.composerDisabled) {
+            return undefined;
+        }
+        return this.props.chatWindow.autofocus;
+    }
+
+    get autofocusThread() {
+        const autofocus = this.props.chatWindow.autofocus;
+        if (this.isMobileOS) {
+            return autofocus + (this.thread.composerDisabled ? 1 : 0);
+        }
+        return this.thread.composerDisabled ? autofocus + 1 : undefined;
+    }
+
     get composerType() {
         if (this.thread.model !== "discuss.channel") {
             return "note";

alkuneemi/Luxara

X-original-commit: 4b3f001a00fc7a67485947b6feea9424147bb310

addons/mail/static/src/core/common/chat_window.js
@@ -64,6 +64,21 @@ export class ChatWindow extends Component {
         useBackButton(() => this.close());
     }
 
+    get autofocusComposer() {
+        if (this.isMobileOS || this.channel.composerHidden) {
+            return undefined;
+        }
+        return this.props.chatWindow.autofocus;
+    }
+
+    get autofocusThread() {
+        const autofocus = this.props.chatWindow.autofocus;
+        if (this.isMobileOS) {
+            return autofocus + (this.channel.composerHidden ? 1 : 0);
+        }
+        return this.channel.composerHidden ? autofocus + 1 : undefined;
+    }
+
     get hasActionsMenu() {
         const partition = this.threadActions.partition;
         return (