MoLeap/odooo

[IMP] mass_mailing: minor editor fine-tuning

addons/mass_mailing/static/src/builder/font_plugin.js
@@ -1,0 +1,15 @@
+import { FontPlugin } from "@html_editor/main/font/font_plugin";
+import { registry } from "@web/core/registry";
+
+const excludedFontItems = ["h5", "h6"];
+
+export class MassMailingFontPlugin extends FontPlugin {
+    resources = {
+        ...this.resources,
+        font_items: this.resources.font_items.filter(
+            (item) => !excludedFontItems.includes(item.object.tagName)
+        ),
+    };
+}
+
+registry.category("mass_mailing-plugins").add(MassMailingFontPlugin.id, MassMailingFontPlugin);

addons/mass_mailing/static/src/builder/mass_mailing_builder.js
@@ -35,6 +35,7 @@ export class MassMailingBuilder extends Component {
         const builderProps = Object.assign({}, this.props.builderProps);
         const pluginsToRemove = [
             "BuilderFontPlugin", // Makes call to Google API (can't be used for emails)
+            "FontPlugin", // Replaced by MassMailingFontPlugin
             "SavePlugin",
             "AnchorPlugin",
             "ColorUIPlugin",

alkuneemi/Luxara

addons/mass_mailing/static/src/builder/font_plugin.js
@@ -1,0 +1,15 @@
+import { FontTypePlugin } from "@html_editor/main/font/font_type_plugin";
+import { registry } from "@web/core/registry";
+
+const excludedFontItems = ["h5", "h6"];
+
+export class MassMailingFontPlugin extends FontTypePlugin {
+    resources = {
+        ...this.resources,
+        font_type_items: this.resources.font_type_items.filter(
+            (item) => !excludedFontItems.includes(item.object.tagName)
+        ),
+    };
+}
+
+registry.category("mass_mailing-plugins").add(MassMailingFontPlugin.id, MassMailingFontPlugin);

addons/mass_mailing/static/src/builder/mass_mailing_builder.js
@@ -35,6 +35,7 @@ export class MassMailingBuilder extends Component {
         const builderProps = Object.assign({}, this.props.builderProps);
         const pluginsToRemove = [
             "BuilderFontPlugin", // Makes call to Google API (can't be used for emails)
+            "FontTypePlugin", // Replaced by MassMailingFontPlugin
             "SavePlugin",
             "AnchorPlugin",
             "ColorUIPlugin",