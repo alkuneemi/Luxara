0868596be662d5a2f117811e6568df1d7fa58dd2
alkuneemi <alkuneemi@gmail.com>
2026-05-19 05:43:49 +0300

    feat: رفع insurance_broker_suite/models/__init__.py

---
+++ custom_addons/insurance_broker_suite/models/__init__.py
@@ -0,0 +1,16 @@
+from . import insurance_industry_tag
+from . import insurance_category
+from . import insurance_type
+from . import insurance_subtype
+from . import insurance_company
+from . import insurance_application
+from . import insurance_client
+from . import insurance_policy
+from . import insurance_rfq
+from . import insurance_rfq_quote
+from . import insurance_claim
+from . import insurance_commission
+from . import insurance_opportunity
+from . import sahab_ai_config
+from . import crm_lead_funnel
+
